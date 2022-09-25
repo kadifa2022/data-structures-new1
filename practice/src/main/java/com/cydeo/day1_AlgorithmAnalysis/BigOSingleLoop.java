1106bcf4e3e837bff63a900da1c2bcbfbd017918
Kadifa <Kudickadifa@gmail.com>
2022-09-25 08:07:13 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/day1_AlgorithmAnalysis/BigOSingleLoop.java
@@ -0,0 +1,16 @@
+package com.cydeo.day1_AlgorithmAnalysis;
+
+public class BigOSingleLoop {
+    public static void main(String[] args) {
+        long numberOfOperations=0;
+        int n= 10000;
+        int m=500;
+        long startTime;
+        long endTime;
+        System.out.println("input size n is = " +n);
+
+    }
+
+
+
+}
---
+++ practice/src/main/java/com/cydeo/day1_AlgorithmAnalysis/SunsetViews.java
@@ -0,0 +1,58 @@
+package com.cydeo.day1_AlgorithmAnalysis;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class SunsetViews {
+    public static void main(String[] args) {
+        int[] buildings ={3,5,4,4,3,1,3,2};
+        sunsetViews(buildings,"West").forEach(System.out::println);
+
+    }
+    public static ArrayList<Integer>sunsetViews(int[] buildings, String direction){
+        Stack<Integer> stack= new Stack<>();//stack to hold building indicies that can see sunset
+        int i =0;
+        int step =1;
+        //this part is for west
+        if(direction.equalsIgnoreCase("West")){
+            i = buildings.length-1;
+            step=-1;
+        }
+
+        while (i>=0 && i< buildings.length) {
+            while(!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
+                stack.pop();
+            }
+            stack.push(i);
+            i+=step;
+        }
+        //now I have stack with the building indicies that can see sunset
+        if (direction.equalsIgnoreCase("west")) Collections.reverse(stack);
+
+
+        return new ArrayList<Integer>(stack);
+
+    }
+
+
+
+}
+    /*
+    Given an array of buildings and a direction that all of the buildings face,
+    return an array of the indices of the buildings that can
+    see the sunset. A building can see the sunset if it's
+    strictly taller than all of the buildings that come after
+    it in the direction that it faces. The input array named buildings
+    contains positive, non-zero integers representing the heights of
+    the buildings. A building at index i thus has a height denoted
+    by buildings[i] . All of the buildings face the same direction,
+    and this direction is either east or west, denoted by the input
+    string named direction , which will always be equal to either "EAST" or "WEST" .
+     In relation to the input array, you can interpret these directions as right for east and left for west.
+      Important note: the indices in the output array should be sorted in ascending order.
+    Sample Input buildings = [3, 5, 4, 4, 3, 1, 3, 2]
+    direction = "EAST" Sample Output [1, 3, 6, 7] //
+
+     */
