d34165f8e4cb796680f29574e7f85dc56ac46ebb
Kadifa <Kudickadifa@gmail.com>
2022-09-28 06:24:32 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/day05_stack/PrintMiddleLL/MySinglyLinkedList.java
@@ -0,0 +1,26 @@
+package com.cydeo.day05_stack.PrintMiddleLL;
+
+public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){
+        return head==null;
+    }
+
+    void add(int data){
+        //create new node object from data
+        Node node= new Node(data);
+        if (isEmpty()) {//if the list isEmpty
+            head=tail=null;
+            size++;
+        }else{//if there are elements in the list
+            tail.next=node;
+            tail=node;
+            size++;
+        }
+    }
+
+
+}
