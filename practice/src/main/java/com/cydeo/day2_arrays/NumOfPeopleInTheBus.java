6acac95fa3f709db1301902035415b69824fb311
Kadifa <Kudickadifa@gmail.com>
2022-10-01 21:16:31 -0500

    practice num of the people in the bus

---
+++ practice/src/main/java/com/cydeo/day2_arrays/NumOfPeopleInTheBus.java
@@ -0,0 +1,25 @@
+package com.cydeo.day2_arrays;
+
+import java.util.ArrayList;
+
+public class NumOfPeopleInTheBus {
+
+    public static void main(String[] args) {
+        ArrayList<int[]>busStops=new ArrayList<>();
+        busStops.add(new int[]{10,0});
+        busStops.add(new int[] {3,5});
+        busStops.add(new int[] {2,5});
+        System.out.println(countPassengers(busStops));
+
+    }
+
+    public static int countPassengers(ArrayList<int[]>stops){
+        int numOfPeople=0;
+        for (int i = 0; i < stops.size(); i++) {
+            numOfPeople=numOfPeople+stops.get(i)[0]-stops.get(i)[1];
+
+
+        }
+        return numOfPeople;
+    }
+}
