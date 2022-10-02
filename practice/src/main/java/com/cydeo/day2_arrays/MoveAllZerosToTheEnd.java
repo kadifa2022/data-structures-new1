692c7b38ccaf03c442544200c8de97efab614c07
Kadifa <Kudickadifa@gmail.com>
2022-10-02 09:49:36 -0500

    practice move all zeros to the end

---
+++ practice/src/main/java/com/cydeo/day2_arrays/MoveAllZerosToTheEnd.java
@@ -0,0 +1,46 @@
+package com.cydeo.day2_arrays;
+
+import java.util.Arrays;
+
+public class MoveAllZerosToTheEnd {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(moveAllZerosToTheEnd(new int[]{1,0,2,0,0,7})));
+    }
+    //[1,0,2,0,0,7]
+
+    public static int [] moveAllZerosToTheEnd(int [] array){
+        //create ew array
+        //for loop
+        int counter=0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                counter++;
+            }
+        }
+            int[] arrayForNotZero=new int[array.length-counter];
+            int indexOf=0;
+            for (int j = 0; j < array.length; j++) {
+                if(array[j]!=0){
+                    arrayForNotZero[indexOf]=array[j];
+                    indexOf++;
+                }
+            }
+
+           int[] zero=new int[counter];
+            for (int i = 0; i < zero.length; i++) {
+                zero[i]=0;
+
+            }
+            int [] combine=new int[array.length];
+
+            combine= Arrays.copyOf(arrayForNotZero,array.length);
+
+
+            //combine=Arrays.copyOf(zero, zero.length);
+
+
+
+        return combine;
+    }
+
+}
