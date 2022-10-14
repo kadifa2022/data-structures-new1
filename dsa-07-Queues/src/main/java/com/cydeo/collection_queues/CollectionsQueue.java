e7e9ca03952e7aa1ce2d66e95abd74459e0a1a9b
Kadifa <Kudickadifa@gmail.com>
2022-10-14 13:48:33 -0500

    practice queue list

---
+++ dsa-07-Queues/src/main/java/com/cydeo/collection_queues/CollectionsQueue.java
@@ -0,0 +1,12 @@
+package com.cydeo.collection_queues;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class CollectionsQueue {
+    public static void main(String[] args) {
+
+    }
+}
