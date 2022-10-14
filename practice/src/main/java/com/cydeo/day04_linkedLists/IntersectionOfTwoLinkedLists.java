a5a9eba28e977f4b6608fbb0821ef6ef8e1dca8d
Kadifa <Kudickadifa@gmail.com>
2022-10-13 22:21:24 -0500

    practice linked list

--- practice/src/main/java/com/cydeo/day04_linkedLists/IntersectionOfTwoLinkedLists.java
+++ practice/src/main/java/com/cydeo/day04_linkedLists/IntersectionOfTwoLinkedLists.java
@@ -2,5 +2,25 @@ package com.cydeo.day04_linkedLists;
 
 public class IntersectionOfTwoLinkedLists{
 
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        ListNode first=headA;
+        ListNode second= headB;
+
+        while (first != second){
+            if(first ==null){
+                first=headB;
+            }else{
+                first=first.next;
+            }
+            if (second==null){
+                second=headA;
+            }else{
+                second=second.next;
+            }
+        }
+        return  first;
+    }
+
+
 
 }
