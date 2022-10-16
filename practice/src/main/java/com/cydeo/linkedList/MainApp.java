2a639a16bde927a49f284b5d395df476e7a9503b
Kadifa <Kudickadifa@gmail.com>
2022-10-15 22:09:25 -0500

    practice queue

---
+++ practice/src/main/java/com/cydeo/linkedList/MainApp.java
@@ -0,0 +1,8 @@
+package com.cydeo.linkedList;
+
+public class MainApp {
+    public static void main(String[] args) {
+        LinkedListDemo cl = new LinkedListDemo();
+        System.out.println(cl);//cl is a address
+    }
+}
