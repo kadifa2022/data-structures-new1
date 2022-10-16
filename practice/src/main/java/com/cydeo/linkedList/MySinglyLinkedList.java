7cbe4fe546df454bf7b2884950b3f522cf100882
Kadifa <Kudickadifa@gmail.com>
2022-10-16 18:08:18 -0500

    practice linked list

--- practice/src/main/java/com/cydeo/linkedList/MySinglyLinkedList.java
+++ practice/src/main/java/com/cydeo/linkedList/MySinglyLinkedList.java
@@ -5,31 +5,35 @@ public class MySinglyLinkedList {
     Node tail;
     int size;
 
-    boolean isEmpty(){
-        return head==null;
+    boolean isEmpty() {
+        return head == null;
     }
-    void add(int data){
+
+    void add(int data) {
         //create a new object from data
 
-        Node node=new Node(data);
-        if(isEmpty()){//if the list empty
-            head=tail=node;
+        Node node = new Node(data);
+        if (isEmpty()) {//if the list empty
+            head = tail = node;
             size++;
-        }else{//if there are elements in list
-            tail.next=node;
-            tail=node;
+        } else {//if there are elements in list
+            tail.next = node;
+            tail = node;
             size++;
 
         }
     }
-    void printNodes(){
-        Node current=head;
-        while(current!=null){
-            if(current.next==null) System.out.println(current.id + "=>null");
-            System.out.println(current.id + "=>");
-            current=current.next;
+
+    void printNodes() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null) System.out.println(current.id + "=>null");
+            else {
+                System.out.print(current.id + "=>");
+            }
+            current = current.next;
         }
+
+
     }
-
-
 }
