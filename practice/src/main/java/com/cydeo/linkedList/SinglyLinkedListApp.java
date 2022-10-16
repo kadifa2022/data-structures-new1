721607ab27c89308e26e6687e22f7e94d6a338bd
Kadifa <Kudickadifa@gmail.com>
2022-10-16 18:02:09 -0500

    practice linked list

--- practice/src/main/java/com/cydeo/linkedList/SinglyLinkedListApp.java
+++ practice/src/main/java/com/cydeo/linkedList/SinglyLinkedListApp.java
@@ -2,7 +2,7 @@ package com.cydeo.linkedList;
 
 public class SinglyLinkedListApp {
     public static void main(String[] args) {
-        MySinglyLinkedList  myList= new MySinglyLinkedList();
+        MySinglyLinkedList  myList= new MySinglyLinkedList();//is empty
         System.out.println(myList.isEmpty());
     }
 }
