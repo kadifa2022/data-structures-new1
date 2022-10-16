65bd24ce57c1c3511af03348ef712fdbcaffc4c3
Kadifa <Kudickadifa@gmail.com>
2022-10-15 22:46:14 -0500

    practice linked list

--- practice/src/main/java/com/cydeo/linkedList/MainApp.java
+++
@@ -1,8 +0,0 @@
-package com.cydeo.linkedList;
-
-public class MainApp {
-    public static void main(String[] args) {
-        LinkedListDemo cl = new LinkedListDemo();
-        System.out.println(cl);//cl is a address
-    }
-}
---
+++ practice/src/main/java/com/cydeo/linkedList/MySinglyLinkedList.java
@@ -0,0 +1,12 @@
+package com.cydeo.linkedList;
+
+public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){
+        return head==null;
+    }
+
+}
