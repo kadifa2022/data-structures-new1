3523697aba19bd8cd16bc51f36b80c6a4fcb17f8
Kadifa <Kudickadifa@gmail.com>
2022-10-15 22:46:07 -0500

    practice linked list

---
+++ practice/src/main/java/com/cydeo/linkedList/SinglyLinkedListApp.java
@@ -0,0 +1,8 @@
+package com.cydeo.linkedList;
+
+public class SinglyLinkedListApp {
+    public static void main(String[] args) {
+        MySinglyLinkedList  myList= new MySinglyLinkedList();
+        System.out.println(myList.isEmpty());
+    }
+}
