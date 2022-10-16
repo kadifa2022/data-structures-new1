cab276d55e63f7d65b3bdfd340c2226da209259f
Kadifa <Kudickadifa@gmail.com>
2022-10-15 22:09:07 -0500

    practice queue

--- dsa-07-Queues/src/main/java/com/cydeo/collection_queues/CollectionsQueue.java
+++ dsa-07-Queues/src/main/java/com/cydeo/collection_queues/CollectionsQueue.java
@@ -7,6 +7,19 @@ import java.util.TreeMap;
 
 public class CollectionsQueue {
     public static void main(String[] args) {
+        MyQueue<Integer> mq=new MyQueue<>();
+        mq.enqueue(1);
+        mq.enqueue(2);
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        Queue<Integer> cq=new LinkedList<>();
+        cq.add(1);
+        cq.add(3);
+        System.out.println(cq.remove());
+        System.out.println(cq.remove());
+
+      //  HashMap<String, Integer> hashMap= new HashMap<>();
+      //  TreeMap<String, Integer> treeMap=new TreeMap<>();
 
     }
 }
