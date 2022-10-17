cb8c0905161a540267199e0ae05e0642cb51e193
Kadifa <Kudickadifa@gmail.com>
2022-10-17 08:40:08 -0500

    practice linked list

---
+++ practice/src/main/java/com/cydeo/day04_linkedLists/Assignements/IndexOfMethod.java
@@ -0,0 +1,16 @@
+package com.cydeo.day04_linkedLists.Assignements;
+
+public class IndexOfMethod {
+
+    int indexOf(int id){
+        //write your code
+
+        return -1;
+    }
+
+}
+/*
+1.Find the indexOf sn element and return it.//position
+2. if not found return -1.
+
+ */
