2df02f7f3801b5eda0f307ac90ab86208a85efc6
Kadifa <Kudickadifa@gmail.com>
2022-10-30 18:49:02 -0500

    practice Trees

--- dsa-08-Trees/src/main/java/TNode.java
+++ dsa-08-Trees/src/main/java/TNode.java
@@ -1,9 +1,11 @@
 public class TNode {
+    //create 3 node
     int value;
     TNode leftChild;
     TNode rightChild;
 
-    public TNode(int value) {
+    public TNode(int value) { //constructor with value
         this.value = value;
     }
+
 }
