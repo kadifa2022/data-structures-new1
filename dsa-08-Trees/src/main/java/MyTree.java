69d4bfd69bdbe617d5ebf3ee6a4c2ce235ccd4ed
Kadifa <Kudickadifa@gmail.com>
2022-10-30 18:48:55 -0500

    practice Trees

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -1,24 +1,28 @@
 public class MyTree {
 
+    // need just rood
     TNode root;
-    public MyTree(){
+    public MyTree(){//NoArgsConstructor
     }
-    void insert(int value) {
-        TNode newNode = new TNode(value);
+    void insert(int value) {//start with root with void
+        TNode newNode = new TNode(value);//TN assign with value new TNode
+
         if (root == null) {
             root = newNode;
             return;
         }
         TNode current = root;
         while (true) {
-            if (value <= current.value) {
-                if (current.leftChild == null) {
+            if (value <= current.value) {//value can  be less than value
+                if (current.leftChild == null) {//another check
+                    // if left is null insert there!!
                     current.leftChild = newNode;
                     break;
                 }
-                current = current.leftChild;
-            } else {
+                current = current.leftChild;//assign
+            } else {//value is grater than current value
                 if (current.rightChild == null) {
+                    //if right is null insert there!!
                     current.rightChild = newNode;//inserted child
                     break;
                 }
