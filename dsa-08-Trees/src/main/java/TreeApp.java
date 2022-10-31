c45c9af2c112b01e1b67ba94c2af1e3bf01d0bf8
Kadifa <Kudickadifa@gmail.com>
2022-10-30 21:16:51 -0500

    practice Trees

--- dsa-08-Trees/src/main/java/TreeApp.java
+++ dsa-08-Trees/src/main/java/TreeApp.java
@@ -1,14 +1,26 @@
 public class TreeApp {
     public static void main(String[] args) {
 
-
         MyTree tree = new MyTree();
-        int[] numbers = new int[]{10, 5, 12, 3, 6, 11, 13, 2,};
+        int[] numbers = new int[] {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
 
-        for (int i = 0; i < 0; i++) {
+        for (int i = 0; i < 11; i++) {
             tree.insert(numbers[i]);
         }
-        //VisualiseTree.printTree(tree.root, null, false);
+
+        VisualizeTree.printTree(tree.root, null, false);
+
+        tree.preOrderTraversal(tree.root);
+       System.out.println();
+        System.out.println("In Order Traversal:");
+        System.out.println(tree.root);
+        System.out.println();
+        System.out.println("Post Order Traversal: ");
+        tree.postOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("Level Order Traversal: ");
+        tree.levelOrderTraversal();
+
 
 
 
