71fa719841f94881b434ee543094d4e45d0e6839
Kadifa <Kudickadifa@gmail.com>
2022-11-09 11:45:44 -0600

    isPalindrome

---
+++ practice/src/main/java/com/cydeo/day04_linkedLists/IsPalindrome.java
@@ -0,0 +1,31 @@
+package com.cydeo.day04_linkedLists;
+
+import java.util.Locale;
+
+public class IsPalindrome {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(String word) {
+        if (word.isEmpty() || word == null) {
+            return false;
+        }
+        word = word.replace(" ", "").toLowerCase();
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) == word.charAt(right)) {
+                right--;
+                left++;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+}
