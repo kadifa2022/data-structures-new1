59f0357fa9a9a75be7d327622cf5a6a0a5234d5f
Kadifa <Kudickadifa@gmail.com>
2022-11-09 11:47:33 -0600

    valid anagram

---
+++ practice/src/main/java/com/cydeo/queues/ValidAnagram.java
@@ -0,0 +1,35 @@
+package com.cydeo.queues;
+
+
+
+
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+
+        System.out.println();
+        isAnagram("Anagram ", "nagaram");
+
+
+    }
+
+          public static boolean isAnagram(String s, String t){
+              //al
+              int [] letters= new int[26];
+              char[] arr1=s.toCharArray();
+              char[] arr2=t.toCharArray();
+
+              for (int i = 0; i < arr1.length; i++) {
+                  letters[arr1[i] -'a'] ++;
+              }
+              for (int i = 0; i < arr2.length; i++) {
+                  letters[arr2[i] - 'a'] --;
+
+              }
+              for (int letter : letters){
+                  if(letter !=0) return false;
+              }
+
+              return true;
+    }
+}
