5bcb008244cc1f09e6bc2469135ab6513183b0c4
Kadifa <Kudickadifa@gmail.com>
2022-11-09 20:48:06 -0600

    valid anagram arrays

--- practice/src/main/java/com/cydeo/hashMap/ValidAnagram.java
+++ practice/src/main/java/com/cydeo/hashMap/ValidAnagram.java
@@ -1,11 +1,13 @@
 package com.cydeo.hashMap;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ValidAnagram {
     public static void main(String[] args) {
 
         System.out.println(validAnagram("anagram", "gramana"));
+        System.out.println(isAnagram("car", "rac"));
 
     }
 
@@ -43,6 +45,14 @@ public class ValidAnagram {
         }
         return map1.isEmpty();
     }
+    public static boolean isAnagram(String s, String t){
+        char[] chars=s.toCharArray();
+        char[] chars1=t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chars1);
+        return Arrays.equals(chars,chars1);
+    }
+
 
 
 }
