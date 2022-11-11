f8a15301d599fce3775b88b3434b862139fe307b
Kadifa <Kudickadifa@gmail.com>
2022-11-10 18:36:29 -0600

    dequeue practice

--- practice/src/main/java/com/cydeo/queues/queueImplementation/CollectionsQueue.java
+++ practice/src/main/java/com/cydeo/queues/queueImplementation/CollectionsQueue.java
@@ -4,6 +4,9 @@ public class CollectionsQueue {
 
     public static void main(String[] args) {
         MyQueue <Integer> mq = new MyQueue();
+        mq.enqueue(1);
+        mq.enqueue(2);
+        System.out.println(mq.dequeue());
 
     }
 }
