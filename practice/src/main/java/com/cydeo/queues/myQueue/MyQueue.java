55801254a9737e30b59c018148ce62a7ff40c11f
Kadifa <Kudickadifa@gmail.com>
2022-11-10 18:41:07 -0600

    dequeue practice

--- practice/src/main/java/com/cydeo/queues/myQueue/MyQueue.java
+++ practice/src/main/java/com/cydeo/queues/myQueue/MyQueue.java
@@ -8,30 +8,32 @@ public class MyQueue {
     Stack<Integer> stack2=new Stack<>();
 
 
-
-
-
-
-
-
     void enqueue(int num){
         stack1.push(num);
 
 
     }
+
+
     int dequeue(){
+        peek();
 
-
-        return  0;
+        return  stack2.pop();
 
 
     }
     int peek(){
-        return 0;
+        if(stack2.isEmpty())
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        return stack2.peek();
+
     }
 
+
     boolean isEmpty(){
-        return true;
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 
 }
