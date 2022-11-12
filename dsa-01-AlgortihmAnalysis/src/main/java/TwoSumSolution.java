ba34871e8a376f5ebcfa0e317a84997c5e11fa95
Kadifa <Kudickadifa@gmail.com>
2022-11-11 21:43:05 -0600

    two sum practice

---
+++ dsa-01-AlgortihmAnalysis/src/main/java/TwoSumSolution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSumSolution {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 7, 9, 11};
+        System.out.println(Arrays.toString(twoSumSolution(array, 9)));
+
+    }
+
+    public static int[] twoSumSolution(int[] array, int targetValue) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (i != j) {
+                    if (array[i] + array[j] == targetValue) return new int[]{i, j};
+
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
