294cafde54e87feb375da3b9697873263e7139fa
Kadifa <Kudickadifa@gmail.com>
2022-11-16 22:52:28 -0600

    longest valid parentheses

--- practice/src/main/java/com/cydeo/tree/InOrderTraversal.java
+++ practice/src/main/java/com/cydeo/tree/InOrderTraversal.java
@@ -1,6 +1,6 @@
 package com.cydeo.tree;
 
-import javax.swing.tree.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -20,16 +20,22 @@ public class InOrderTraversal {
 
 
         TreeNode root = new TreeNode(10);
+
         root.left = new TreeNode(4);
         root.right = new TreeNode(15);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(5);
+
+        System.out.println(inOrderTraversal(root.left));
+        System.out.println(inOrderTraversal(root.right));
+
 
     }
 
 
 
     public static List<Integer> inOrderTraversal(TreeNode root){
+             //3 4 5 10 15
        TreeNode current = root;
        Stack<TreeNode> stack = new Stack<>();
        List<Integer> list= new ArrayList<>();
