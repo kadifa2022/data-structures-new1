76e804f3c98ad5e9c49c5c442dc8676c75cc57de
Kadifa <Kudickadifa@gmail.com>
2022-11-19 08:47:23 -0600

    node Depth Sum

---
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -0,0 +1,105 @@
+import java.util.NoSuchElementException;
+
+public class MyHeap {
+
+    int [] items;
+    int size;
+
+    public MyHeap(int capacity) {
+        this.items = new int [capacity];
+        this.size=0; //re-set size
+    }
+
+    public int peek(){//will not remove root
+        if(size==0) throw new NoSuchElementException();
+        return items[0];
+
+    }
+    public void insert(int value){//if is array-full throw exception else ++ to array
+        if(size==items.length)throw new IllegalStateException();
+        else{
+            items[size++]=value;
+            bubbleUp();
+
+        }
+    }
+
+    public void bubbleUp(){
+        //calculate start point index
+        int index=size-1;
+        //compare new inserted value in proper place
+        //while index is >0 %% parent is less than child continue
+        //1.swap child and parent
+        //calculate next index=parent index
+        while (index>0 && items[index]>items[parentIndex(index)]){
+            //swap()
+            swap(index, parentIndex(index));
+            //assign index with the parent index
+            index=parentIndex(index);//like current=current.next
+
+        }
+
+            }
+
+    public int remove(){
+        if(size==0) throw new NoSuchElementException();
+        else{
+            int result=items[0];
+            items[0]=items[--size];
+            bubbleDown();
+            return result;
+
+        }
+    }
+    public void bubbleDown(){
+        int index=0;
+        int largerChildIndex;
+        while (index<=size && !isValidParent(index)){
+            largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index=largerChildIndex;
+
+        }
+    }
+    public boolean isValidParent(int index){
+        if(!hasLeftChild(index)) return true;
+        else{
+         boolean isValid=false;
+            if(hasRightChild(index)){
+                isValid=(items[index]>=items[leftChildIndex(index)] && items[index]>=items[rightChildIndex(index)]);
+                 }
+            return isValid;
+        }
+
+    }
+
+
+    public void swap(int first, int second){//swap method
+        int temp=items[first];
+        items[first]=items[second];
+        items[second]=temp;
+    }
+    public int parentIndex(int index){ return (index-1)/2;
+    }
+    public boolean hasLeftChild(int index){return leftChildIndex(index)<=size; }
+     public boolean hasRightChild(int index){return rightChildIndex(index)<=size; }
+    public int leftChildIndex(int index) {return index*2+1; }
+     public int rightChildIndex(int index) {return index*2+2; }
+    public int largerChildIndex(int index){
+        if(!hasLeftChild(index)) return index;
+        else if(!hasRightChild(index)){
+            return leftChildIndex(index);
+        }
+        return   items[leftChildIndex(index)]>items[rightChildIndex(index)] ? leftChildIndex(index):rightChildIndex(index);
+
+    }
+
+
+    public void printHeap(){
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i]+ ", ");
+
+        }
+        System.out.println();
+    }
+}
