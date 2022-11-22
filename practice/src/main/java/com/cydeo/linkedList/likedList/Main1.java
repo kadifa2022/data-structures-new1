abbfeac0f93942d3dae17ef68d436c95262a5714
Kadifa <Kudickadifa@gmail.com>
2022-11-22 15:44:50 -0600

    node Depth Sum

---
+++ practice/src/main/java/com/cydeo/linkedList/likedList/Main1.java
@@ -0,0 +1,22 @@
+package com.cydeo.linkedList.likedList;
+
+import java.util.LinkedList;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        LinkedList<String> x=new LinkedList<>();
+        x.removeFirst();
+    var list = new LinkedList();
+    list.addLast(10);
+    list.addLast(20);
+    list.addLast(30);
+    list.removeFirst();
+    list.removeLast();
+        System.out.println(list.indexOf(30));
+        System.out.println(list);
+        System.out.println(list.contains(10));
+
+
+    }
+}
