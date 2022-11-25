ac96ebc5ecc591b7d27a29a57b0b22b553649fa7
Kadifa <Kudickadifa@gmail.com>
2022-11-25 11:23:09 -0600

    Two sum and treemap

--- practice/src/main/java/com/cydeo/day2_arrays/frequencyAndConvertingToWord/FindFrequencyOfEachArrayElementAndConvertToEnglishWord.java
+++ practice/src/main/java/com/cydeo/day2_arrays/frequencyAndConvertingToWord/FindFrequencyOfEachArrayElementAndConvertToEnglishWord.java
@@ -34,3 +34,13 @@ public class FindFrequencyOfEachArrayElementAndConvertToEnglishWord {
 //
 
     }
+/*
+Write a function that takes an integer array, counts each number and prints a sentence to say how many times a number is repeated. Number order is not important.
+EXAMPLE:
+input : array = {1, 2, 3, 4, 3, 2, 1, 3, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2} output :
+1 is two times
+3 is three times
+4 is two times
+2 is more than ten times
+
+ */
