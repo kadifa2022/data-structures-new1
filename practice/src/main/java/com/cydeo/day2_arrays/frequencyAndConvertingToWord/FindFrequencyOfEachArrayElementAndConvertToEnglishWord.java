bd6e4ebd8c07f583ae8114e068d4ff695491cd82
Kadifa <Kudickadifa@gmail.com>
2022-11-25 10:38:54 -0600

    Two sum and treemap

---
+++ practice/src/main/java/com/cydeo/day2_arrays/frequencyAndConvertingToWord/FindFrequencyOfEachArrayElementAndConvertToEnglishWord.java
@@ -0,0 +1,36 @@
+package com.cydeo.day2_arrays.frequencyAndConvertingToWord;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFrequencyOfEachArrayElementAndConvertToEnglishWord {
+    public static void main(String[] args) {
+
+    }
+//
+//    static void arrayAndMap(int[] arr){
+//        if (arr == null || arr.length==0);
+//        System.out.println("array is empty");
+//        return;
+//    }
+//    String[] words={"zero","one", "two", "three","four","five", "six","seven",
+//            "eight","nine", "ten", "more than ten"};
+//    Map<Integer, String> map = new HashMap<>();
+//
+//    for (int each : arr) {
+//        int count =0;
+//        for (int n: arr) {
+//            if(each==n){
+//                count++;
+//            }
+//        }
+//        if (count > 10) {
+//            map.put(each, words[11]);
+//        }else{
+//            map.put(each,words[count]);
+//        }
+//    }
+//    map.forEach((k, v)-> System.out.println(k + "is" + v +"times"));
+//
+
+    }
