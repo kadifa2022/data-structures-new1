38d38eb2f4411f0d5399a0ff7c37fd66121e9aee
Kadifa <Kudickadifa@gmail.com>
2022-11-25 11:09:14 -0600

    Two sum and treemap

--- practice/src/main/java/com/cydeo/day2_arrays/frequencyAndConvertingToWord/WithStreamAndEnum.java
+++ practice/src/main/java/com/cydeo/day2_arrays/frequencyAndConvertingToWord/WithStreamAndEnum.java
@@ -1,5 +1,28 @@
 package com.cydeo.day2_arrays.frequencyAndConvertingToWord;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class WithStreamAndEnum {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    static void withStreamAndEnum(int[] arr){
+        if(arr==null || arr.length==0){
+            System.out.println("array is empty");
+            return;
+        }
+        Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(i->i, Collectors.counting()))
+                .forEach((k,v)->{
+            long n=v<=11 ? v:11;
+                    System.out.println(k + " is " +NumberToWord.values()[(int) n].value + "times");
+        });
+    }
 
 }
