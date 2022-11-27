e38b18981fe657788e2f87fa6d6f9b1cebac9e90
Kadifa <Kudickadifa@gmail.com>
2022-11-26 18:52:26 -0600

    linked list cycle

---
+++ practice/src/main/java/com/cydeo/day04_linkedLists/cycleDetectionProblem/ListNode.java
@@ -0,0 +1,14 @@
+package com.cydeo.day04_linkedLists.cycleDetectionProblem;
+
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x){
+        val=x;
+        next=null;
+    }
+
+}
