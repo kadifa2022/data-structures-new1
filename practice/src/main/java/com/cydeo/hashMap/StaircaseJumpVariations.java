107996f861618b958264be6018a4f91fd9419854
Kadifa <Kudickadifa@gmail.com>
2022-11-26 18:52:32 -0600

    linked list cycle

---
+++ practice/src/main/java/com/cydeo/hashMap/StaircaseJumpVariations.java
@@ -0,0 +1,37 @@
+package com.cydeo.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StaircaseJumpVariations {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+
+    }
+//
+//    public static int map_main(int n){
+//        Map<Integer, Integer> map = new HashMap<>();
+//        return mapUtil(n,map);
+//
+//    }
+//
+//    private static int mapRecursive(int n, Map<Integer, Integer> map){
+//        if(n < 0) throw new IllegalStateException("stair numbers can't be less than zero");
+//
+//        if(n <= 2){
+//            return n;
+//        } else if (n == 3) {
+//            return 4;
+//        }
+//        if(map.containsKey(n))//if solved before the use it
+//            return map.get(n);
+//
+//        int ways= mapUtil(n-1, map)
+//                + mapUtil(n-2, map)
+//                + mapUtil(n-3, map);
+//        map.put(n, ways);
+//        return ways;
+    }
+
