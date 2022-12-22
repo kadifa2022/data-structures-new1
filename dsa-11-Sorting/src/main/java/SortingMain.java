52ad4079787c316b485e9ff44fc03af1e59aedf2
Kadifa <Kudickadifa@gmail.com>
2022-12-22 11:59:59 -0600

    selection sort

--- dsa-11-Sorting/src/main/java/SortingMain.java
+++ dsa-11-Sorting/src/main/java/SortingMain.java
@@ -5,5 +5,7 @@ public class SortingMain {
     public static void main(String[] args) {
         int[] numbers={7,2,4,6,1,9,-1};
         System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
+
+        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
     }
 }
