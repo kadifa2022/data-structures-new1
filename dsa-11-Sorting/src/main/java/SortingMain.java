30759fc7f116a65608251b0b90ebf2a94220c585
Kadifa <Kudickadifa@gmail.com>
2022-12-22 20:26:23 -0600

    JD1Merge sort

--- dsa-11-Sorting/src/main/java/SortingMain.java
+++ dsa-11-Sorting/src/main/java/SortingMain.java
@@ -4,8 +4,9 @@ public class SortingMain {
 
     public static void main(String[] args) {
         int[] numbers={7,2,4,6,1,9,-1};
-        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
-
-        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+//        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
+//        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+        JD1MergeSort.mergeSort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
