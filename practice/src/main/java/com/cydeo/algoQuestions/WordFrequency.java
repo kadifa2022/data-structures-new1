25b1a7b3d51e3693292e8eed5b4d9903b797084c
Kadifa <Kudickadifa@gmail.com>
2022-12-28 18:14:57 -0600

    Word frequency

---
+++ practice/src/main/java/com/cydeo/algoQuestions/WordFrequency.java
@@ -0,0 +1,45 @@
+package com.cydeo.algoQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+       wordFreq();
+
+    }
+
+    public static void wordFreq(){
+
+        String text= "Ann while Bob had had had had had a better effect on on the teacher";
+        Map<String, Integer> freqMap = new HashMap<>();
+
+        asList(text.split(" ")).forEach(s ->{
+            if(freqMap.containsKey(s)){
+                Integer count= freqMap.get(s);
+                freqMap.put(s, count+1);
+            }else{
+                freqMap.put(s, 1);
+
+            }
+
+        });
+        System.out.println(freqMap.toString());
+    }
+
+
+}
+
+
+
+/*
+
+        //Split the sentence into word list
+        //loop on the word list
+        //if hashmap contains the given word, increment the frequency count
+        //else put the word into hashmap and ser its frequency as 1
+
+
+ */
