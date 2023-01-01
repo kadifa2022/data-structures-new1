f729ba903a40aa5d14cee8fc6a9065ce7949f97c
Kadifa <Kudickadifa@gmail.com>
2022-12-31 21:53:42 -0600

    move all zeros to the end

---
+++ practice/src/main/java/com/cydeo/algoQuestions/MoveAllZerosToTheEnd.java
@@ -0,0 +1,23 @@
+package com.cydeo.algoQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MoveAllZerosToTheEnd {
+    public static void main(String[] args) {
+
+    }
+    public static List<Integer> moveZerosToTheEnd(List<Integer> list){
+        int originalSize=list.size();
+        list.removeAll(Arrays.asList(0));
+        int newSize=list.size();
+        int totalNumberOfZeros=originalSize - newSize;
+        for (int i = 0; i <totalNumberOfZeros ; i++) {
+            list.add(0);
+
+        }
+        return list;
+
+    }
+
+}
