64448800e16f7ad77a8ff01f76749fc4bc70ec83
Kadifa <Kudickadifa@gmail.com>
2023-02-07 22:50:15 -0600

    kids with candies

--- dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_1_KidsWithCandies.java
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_1_KidsWithCandies.java
@@ -1,12 +1,15 @@
 package com.cydeo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Session_1_KidsWithCandies {
     public static void main(String[] args){
         int [] candies={2,3,5,1,3};
-        System.out.println();
+        System.out.println(kidsWithCandies(candies, 3).toString());
+
+        System.out.println(kidsWithCandies1(candies,2).toString());
     }
 
     public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
@@ -30,5 +33,16 @@ public class Session_1_KidsWithCandies {
         //return result
         return result;
     }
+
+    public static List<Boolean> kidsWithCandies1(int[] candies, int extraCandies){
+        List<Boolean> result= new ArrayList<>();
+        int max = Arrays.stream(candies).max().getAsInt();
+        for(int candy : candies){
+            result.add(candy+extraCandies>=max);
+        }
+        return result;
+    }
+
+
 
 }
