a538a3588424a4fae725e1f6fb63543bb2c31418
Kadifa <Kudickadifa@gmail.com>
2023-02-24 14:56:38 -0600

    practice

--- dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_8_Trees/PhoneBookTree.java
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_8_Trees/PhoneBookTree.java
@@ -3,11 +3,46 @@ package com.cydeo.Session_8_Trees;
 import java.util.List;
 
 public class PhoneBookTree {
+    CustomerNode root;
 
-    void insert(CustomerNode  newNode){
-
+    public PhoneBookTree() {//constructor
+        this.root =null; //initial root with null when  you create object
     }
-    void printNamesInAscOrder(CustomerNode root){
+
+
+    void insert(CustomerNode newNode){
+
+        if(root == null){ //if is null and assign new node
+            root=newNode; // assign new node to root
+            return;
+        }
+        CustomerNode current = root;
+        while (true) {
+            if (newNode.firstName.compareToIgnoreCase(current.firstName) < 0) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild= newNode;
+                    break;
+
+                }
+                current=current.rightChild;
+
+
+            }
+        }
+    }
+    void printNamesInAscOrder(CustomerNode root){ //traversal in order
+
+        if(root==null) return; // always start from the root -if root null there is nothing
+        printNamesInAscOrder(root.leftChild);
+        System.out.println(root.firstName+ ", ");
+        printNamesInAscOrder(root.rightChild);
+
 
     }
     List<String>  firstNamesList(CustomerNode root){
