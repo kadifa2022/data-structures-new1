af3a2d5c18ae69c9f976391c4886d9e55cae0443
Kadifa <Kudickadifa@gmail.com>
2023-02-24 20:05:26 -0600

    practice

--- dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_8_Trees/PhoneBookTree.java
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/Session_8_Trees/PhoneBookTree.java
@@ -1,19 +1,21 @@
 package com.cydeo.Session_8_Trees;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class PhoneBookTree {
     CustomerNode root;
 
     public PhoneBookTree() {//constructor
-        this.root =null; //initial root with null when  you create object
+        this.root = null; //initial root with null when  you create object
     }
 
 
-    void insert(CustomerNode newNode){
+    void insert(CustomerNode newNode) {
 
-        if(root == null){ //if is null and assign new node
-            root=newNode; // assign new node to root
+        if (root == null) { //if is null and assign new node
+            root = newNode; // assign new node to root
             return;
         }
         CustomerNode current = root;
@@ -26,27 +28,39 @@ public class PhoneBookTree {
                 current = current.leftChild;
             } else {
                 if (current.rightChild == null) {
-                    current.rightChild= newNode;
+                    current.rightChild = newNode;
                     break;
 
                 }
-                current=current.rightChild;
+                current = current.rightChild;
 
 
             }
         }
     }
-    void printNamesInAscOrder(CustomerNode root){ //traversal in order
 
-        if(root==null) return; // always start from the root -if root null there is nothing
+    void printNamesInAscOrder(CustomerNode root) { //traversal in order
+
+        if (root == null) return; // always start from the root -if root null there is nothing
         printNamesInAscOrder(root.leftChild);
-        System.out.println(root.firstName+ ", ");
+        System.out.println(root.firstName + ", ");
         printNamesInAscOrder(root.rightChild);
 
 
     }
-    List<String>  firstNamesList(CustomerNode root){
+
+    List<String> firstNamesList(CustomerNode root) {
         return null;
+
+
+//        Queue<TNode> queue= new LinkedList<>();
+//        queue.add(root);//add root to queue
+//        while(!queue.isEmpty()){//queue is not empty
+//            TNode toVisit=queue.poll();//mean front of element// as variable to print
+//            System.out.print(toVisit.value+", ");//  print out and visit element
+//            if( toVisit.leftChild!=null) queue.add(toVisit.leftChild);//check if we have a left child and put in queue
+//            if(toVisit.rightChild!=null) queue.add(toVisit.rightChild);//check if we have a right child and put in queue
+//        }
     }
 
 }
