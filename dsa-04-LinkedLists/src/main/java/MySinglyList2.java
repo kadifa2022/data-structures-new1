b7799ec943f079aeba91807f6a3c9876408a7ce6
Kadifa <Kudickadifa@gmail.com>
2023-02-26 11:49:47 -0600

    practice

--- dsa-04-LinkedLists/src/main/java/MySinglyList2.java
+++ dsa-04-LinkedLists/src/main/java/MySinglyList2.java
@@ -3,26 +3,36 @@ public class MySinglyList2 {
     Node tail;
     int size;
 
-    boolean isEmpty(){
-        return  head==null; //no element in the list
+    boolean isEmpty() {
+        return head == null; //no element in the list
     }
 
-    void add(int data){ //insertion
+    void add(int data) { //insertion
 
-       Node node = new Node(data);
-       //no elements in list
-       if(isEmpty()){
-           head=tail=node; // assign head and tail to node and increase size
-           size++;
-       }else{//if is no empty
-           tail.next= node;//assign to tail to node and increase size
-           tail = node;
-           size ++;
+        Node node = new Node(data);
+        //no elements in list
+        if (isEmpty()) {
+            head = tail = node; // assign head and tail to node and increase size
+            size++;
+        } else {//if is no empty
+            tail.next = node;//assign to tail to node and increase size
+            tail = node;
+            size++;
 
-       }
-
-
+        }
 
     }
 
+    void printNodes1() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null) System.out.println(current.id+"=> null");
+            else {
+                System.out.print(current.id +"=> ");
+
+            }
+            current = current.next;
+        }
+
+    }
 }
