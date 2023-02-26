4d42fc7603c9cfc70119b56490e4194e8f3e8a39
Kadifa <Kudickadifa@gmail.com>
2023-02-26 13:32:59 -0600

    practice

--- dsa-04-LinkedLists/src/main/java/MySinglyList2.java
+++ dsa-04-LinkedLists/src/main/java/MySinglyList2.java
@@ -15,13 +15,49 @@ public class MySinglyList2 {
             head = tail = node; // assign head and tail to node and increase size
             size++;
         } else {//if is no empty
-            tail.next = node;//assign to tail to node and increase size
+            tail.next = node;//assign tail to node and increase size
             tail = node;
             size++;
 
         }
 
     }
+    void deleteById(int id){
+        // check if is empty
+        if(isEmpty()) System.out.println("List is empty");
+
+
+        //assign temp value  prev and current with the head
+        Node prev = head;
+        Node current= head;
+        while(current!=null){
+            if(current.id == id) {// if there is match
+
+                // case 1: head
+                if(current == head){
+                    head= current.next;
+                    current.next= null;// brake link
+
+                }
+                // case 2: tail
+                else if(current == tail){
+                    tail= prev;
+                    prev.next = null; // ex tail will be eligible for Garbage collection
+                }
+                // case 3: middle
+                else{
+                    prev.next = current.next;
+                    current.next= null;//current will be eligible for Garbage collection
+                }
+                //after deletion decrease size
+                size--;
+            }
+            // move forward with elements in the list
+            prev= current;
+            current= current.next;
+        }
+    }
+
 
     void printNodes1() {
         Node current = head;
