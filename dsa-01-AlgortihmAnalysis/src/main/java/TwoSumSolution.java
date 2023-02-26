1e2a4c8e62247c2f81d620d38bad5dfdcc8d5b28
Kadifa <Kudickadifa@gmail.com>
2023-02-26 17:32:12 -0600

    practice

--- dsa-01-AlgortihmAnalysis/src/main/java/TwoSumSolution.java
+++ dsa-01-AlgortihmAnalysis/src/main/java/TwoSumSolution.java
@@ -4,7 +4,8 @@ public class TwoSumSolution {
 
     public static void main(String[] args) {
         int[] array = new int[]{4, 7, 9, 11};
-        System.out.println(Arrays.toString(twoSumSolution(array, 16)));
+        System.out.println(Arrays.toString(twoSumSolution(array, 20)));
+        System.out.println(Arrays.toString(twoSumSolution2(array, 11)));
 
     }
 
@@ -18,5 +19,18 @@ public class TwoSumSolution {
             }
         }
         return new int[]{};
+    }
+
+    public static int[] twoSumSolution2(int[] array, int targetValue){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j<array.length; j++){
+                if(i !=j){
+                    if(array[i] + array[j]==targetValue)return  new int[]{i,j};
+                }
+            }
+
+        }
+
+            return  new int[]{};
     }
 }
