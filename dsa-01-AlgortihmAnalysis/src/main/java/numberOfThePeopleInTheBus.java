6b3494d23e8e9dd9279a75d3497ec9c0750bb84b
Kadifa <Kudickadifa@gmail.com>
2023-02-26 18:11:44 -0600

    practice

--- dsa-01-AlgortihmAnalysis/src/main/java/numberOfThePeopleInTheBus.java
+++ dsa-01-AlgortihmAnalysis/src/main/java/numberOfThePeopleInTheBus.java
@@ -7,22 +7,34 @@ public class numberOfThePeopleInTheBus {
         ArrayList<int[]> busStop=new ArrayList<int[]>();
 
 
-        busStop.add(new int[]{10,0});
-        busStop.add(new int[]{3,5});
-        busStop.add(new int[]{2,5});
+        busStop.add(new int[]{10,0});// 10 in bus 0 out first stop
+        busStop.add(new int[]{3,5});// 3 in, out 5
+        busStop.add(new int[]{2,5});// 2 in , out 5
 
         System.out.println(countPassengers(busStop));
+        System.out.println(countPassengersInBus(busStop));
 
 
 
     }
 
-    public static int countPassengers(ArrayList<int[]> stops){
+    public static int countPassengers(ArrayList<int[]> stops){// o(n) complexity because is single one loop with size
         int numOfPeople=0;
         for (int i = 0; i < stops.size(); i++) {
             numOfPeople=numOfPeople + stops.get(i)[0]-stops.get(i)[1];
 
         }
         return numOfPeople;
+    }
+
+    public static int countPassengersInBus(ArrayList<int[]> stops){
+        int numOfPeople= 0;//initials variables
+        for (int i = 0; i <stops.size() ; i++) {
+            numOfPeople= numOfPeople + stops.get(i)[0]-stops.get(i)[1];
+
+        }
+        return numOfPeople;
+
+
     }
 }
