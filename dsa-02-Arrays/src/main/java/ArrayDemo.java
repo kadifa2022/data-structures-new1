72247079a992130efb41038309a78fe9f5244774
Kadifa <Kudickadifa@gmail.com>
2023-02-26 20:18:09 -0600

    practice

--- dsa-02-Arrays/src/main/java/ArrayDemo.java
+++ dsa-02-Arrays/src/main/java/ArrayDemo.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayDemo {
     public static void main(String[] args) {
         int[] scoreArray=new int[10];
+        List<String> a = new ArrayList<>();
 
     }
 }
