da8b80312b4d034a67bcef6d799f4a9a68733d5b
Kadifa <Kudickadifa@gmail.com>
2023-03-05 20:17:31 -0600

    practice

---
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/session_10_ValidateBinaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package com.cydeo.session_10_ValidateBinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+
+    TreeNode(int val){
+        this.val= val;
+    }
+}
