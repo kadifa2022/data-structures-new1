374fb7c2fa599fbab3779dae8d9dfb9e74659de7
Kadifa <Kudickadifa@gmail.com>
2023-03-09 09:24:54 -0600

    practice

--- practice/src/main/java/com/cydeo/javaCore/Array_findMaximum.java
+++ practice/src/main/java/com/cydeo/javaCore/Array_findMaximum.java
@@ -6,6 +6,7 @@ Write a function that can find the maximum number from an int Array */
       public static void main(String[] args) {
           int[] myArray= {20,45,6,6,7,8,9,0,0,1,2,2,3,3,3};
           System.out.println(maxValue(myArray));
+          System.out.println(minValue(myArray));
 
       }
     public static int maxValue(int[] n){
@@ -18,6 +19,16 @@ Write a function that can find the maximum number from an int Array */
         }
         return max;
 
+    }
+
+    public static int minValue(int[] number){
+          int min = Integer.MAX_VALUE;
+          for(int each: number){
+              if(each<min){
+                  min = each;
+              }
+          }
+          return min;
     }
 
 
