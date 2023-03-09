6ae9cb23a51577855c41dde2c1031b4fee3edee9
Kadifa <Kudickadifa@gmail.com>
2023-03-09 10:18:25 -0600

    practice

---
+++ practice/src/main/java/com/cydeo/javaCore/MiddleCharacter.java
@@ -0,0 +1,27 @@
+package com.cydeo.javaCore;
+
+import java.util.Scanner;
+
+public class MiddleCharacter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+        if(word.length()%2 !=0){
+            if (word.length() >=3) {
+                System.out.println(word.charAt(word.length()/2));
+
+            }else {
+                System.out.println(word.charAt(0) + "" + word.charAt(0) + "" + word.charAt(0));
+            }
+        }else {
+            if(word.length()>=4){
+                System.out.println(word.charAt(word.length())/2-1 + "" +word.charAt(word.length()/2));
+            }else{
+                System.out.println(word.concat(word));
+            }
+
+            }
+
+    }
+}
