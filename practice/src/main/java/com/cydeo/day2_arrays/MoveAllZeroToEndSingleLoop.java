a3ca6c0f1dca36a91a7d153bc11a3c5518cea8fb
Kadifa <Kudickadifa@gmail.com>
2023-03-11 13:38:37 -0600

    practice

--- practice/src/main/java/com/cydeo/day2_arrays/MoveAllZeroToEndSingleLoop.java
+++ practice/src/main/java/com/cydeo/day2_arrays/MoveAllZeroToEndSingleLoop.java
@@ -6,6 +6,7 @@ public class MoveAllZeroToEndSingleLoop {
     public static void main(String[] args) {
         int[] arr = {1, 0, 2, 0, 0, 7};
         System.out.println(Arrays.toString(moveAllZero(arr)));
+        System.out.println(Arrays.toString(moveAllZeroToTheEnd(arr)));
     }
 
     public static int[] moveAllZero(int[] array) {
@@ -27,4 +28,24 @@ public class MoveAllZeroToEndSingleLoop {
         }
         return array;
     }
+
+    public static int[] moveAllZeroToTheEnd(int[] arr){
+        int fast = 0;
+        int slow = 0;
+        while(fast<arr.length -1){
+            if(arr[slow] !=0){
+                slow++;
+            }
+            if(fast<= slow || arr[fast]==0){
+                fast++;
+            }
+            if(arr[slow] ==0 && arr [fast] !=0){
+                arr[slow] = arr[fast];
+                arr[fast]=0;
+            }
+        }
+        return  arr;
+
+    }
+
 }
