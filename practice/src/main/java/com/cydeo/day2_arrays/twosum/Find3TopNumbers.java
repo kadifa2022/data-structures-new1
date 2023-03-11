531276a6ce9432c0069455bca1256334c3d1e23c
Kadifa <Kudickadifa@gmail.com>
2023-03-11 16:36:53 -0600

    practice

--- practice/src/main/java/com/cydeo/day2_arrays/twosum/Find3TopNumbers.java
+++ practice/src/main/java/com/cydeo/day2_arrays/twosum/Find3TopNumbers.java
@@ -1,11 +1,33 @@
 package com.cydeo.day2_arrays.twosum;
 
 
+import java.util.Arrays;
 
 public class Find3TopNumbers {
 
     public static void main(String[] args) {
-        int[] arr = {10, 10, 12, 4, 5,};
+        int[] array = {10, 10, 12, 4, 5,};
+        System.out.println(Arrays.toString(findTop3Numbers(array)));
+
+
+    }
+    public static int[] findTop3Numbers(int [] array){
+        int min=Integer.MIN_VALUE, mid=Integer.MIN_VALUE, max=Integer.MIN_VALUE;
+        for (int i = 0; i <array.length ; i++) {
+
+            if(array[i]>max){
+                min=mid;
+                mid=max;
+                max=array[i];
+            }else if(array[i]>mid){
+                min=mid;
+                mid=array[i];
+            }else if(array[i]>min){
+                min=array[i];
+            }
+
+        }
+        return new int[] {max,mid,min};
 
 
     }
