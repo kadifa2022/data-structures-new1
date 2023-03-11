e6f75791810df22e023f7c2b73a3df9691b703d9
Kadifa <Kudickadifa@gmail.com>
2023-03-10 20:06:38 -0600

    practice

--- dsa-01-AlgortihmAnalysis/src/main/java/TwoSumSolution.java
+++ dsa-01-AlgortihmAnalysis/src/main/java/TwoSumSolution.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSumSolution {
 
@@ -6,6 +8,7 @@ public class TwoSumSolution {
         int[] array = new int[]{4, 7, 9, 11};
         System.out.println(Arrays.toString(twoSumSolution(array, 20)));
         System.out.println(Arrays.toString(twoSumSolution2(array, 11)));
+
 
     }
 
@@ -33,4 +36,18 @@ public class TwoSumSolution {
 
             return  new int[]{};
     }
+
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> num_map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++) {
+            int complement = target-nums[i];
+            if(num_map.containsKey(complement)){
+                return new int[]{num_map.get(complement), i};
+            }
+            num_map.put(nums[i], i);
+        }
+        throw  new IllegalArgumentException("No match found!");
+
+    }
+
 }
