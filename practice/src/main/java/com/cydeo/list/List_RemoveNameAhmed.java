ef2567e770e95b9ad15dd4c8f6ccedf65301a995
Kadifa <Kudickadifa@gmail.com>
2023-03-14 22:58:47 -0500

    practice

--- practice/src/main/java/com/cydeo/list/List_RemoveNameAhmed.java
+++ practice/src/main/java/com/cydeo/list/List_RemoveNameAhmed.java
@@ -1,9 +1,19 @@
 package com.cydeo.list;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class List_RemoveNameAhmed {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Murat");
+        names.add("Omer");
+        names.add("Yusuf");
+        names.add("Ahmed");
+        System.out.println(removeName(names));
+    }
 
     public static List<String> removeName(List<String> names){
         Iterator<String> it = names.iterator();
