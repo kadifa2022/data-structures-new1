ea2d0c674f3c7735eb160bb2dbe8b475596d3252
Kadifa <Kudickadifa@gmail.com>
2023-03-16 17:45:42 -0500

    practice

---
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/session_13_Graph/Vertex.java
@@ -0,0 +1,10 @@
+package com.cydeo.session_13_Graph;
+
+public class Vertex { // need two parameters
+    int value, weight;
+
+    public Vertex(int value, int weight) {
+        this.value = value;
+        this.weight = weight;
+    }
+}
