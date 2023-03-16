c1e9a287657f65c491f5031decd35b63a73fdae7
Kadifa <Kudickadifa@gmail.com>
2023-03-16 17:45:25 -0500

    practice

---
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/session_13_Graph/Edge.java
@@ -0,0 +1,19 @@
+package com.cydeo.session_13_Graph;
+
+public class Edge {
+    // requested 3 parameters
+    //from vertex to the numbers another vertex
+
+    int from;
+    int to;
+    int weight;
+
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+
+
+}
