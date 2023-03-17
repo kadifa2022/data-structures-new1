0a05cce1ee7d7048cb9674726b4ac96cc9adf2e5
Kadifa <Kudickadifa@gmail.com>
2023-03-17 09:12:57 -0500

    practice

---
+++ dsa-12-practiceForInterview/src/main/java/com/cydeo/session_12_BindaryTree/Node.java
@@ -0,0 +1,13 @@
+package com.cydeo.session_12_BindaryTree;
+
+public class Node {
+    int value;
+    Node leftChild, rightChild;
+    public Node(int value){
+        this.value= value;
+        leftChild= null;
+        rightChild = null;
+
+    }
+
+}
