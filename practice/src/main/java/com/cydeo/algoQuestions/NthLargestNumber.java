ea571e05918732f67fd7aa1d52cc813eb171dae1
Kadifa <Kudickadifa@gmail.com>
2023-03-21 12:39:00 -0500

    practice

--- practice/src/main/java/com/cydeo/algoQuestions/NthLargestNumber.java
+++ practice/src/main/java/com/cydeo/algoQuestions/NthLargestNumber.java
@@ -29,3 +29,12 @@ public class NthLargestNumber {
 
      }
 }
+/*
+write a program that can return the nth largest number from an arraylist
+            ex:
+	            arraylist = {1,2,3,4,5,6,7,7,8,8}
+	            n = 5
+
+            output:
+                4
+ */
