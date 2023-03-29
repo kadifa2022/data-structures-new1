509a4b49d43595ddd0a30a0b7b1e122c98460358
Kadifa <Kudickadifa@gmail.com>
2023-03-29 14:12:12 -0500

    practice

--- practice/src/main/java/com/cydeo/setAndHashSet/SetAndHashSet.java
+++ practice/src/main/java/com/cydeo/setAndHashSet/SetAndHashSet.java
@@ -1,8 +1,6 @@
 package com.cydeo.setAndHashSet;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class SetAndHashSet {
     public static void main(String[] args) {
@@ -18,6 +16,40 @@ public class SetAndHashSet {
         while(namesIterator.hasNext()){
             System.out.println(namesIterator.next());
         }
+
+        List<Integer> numberList= new ArrayList<>();
+        numberList.add(1);
+        numberList.add(2);
+        numberList.add(1);
+        numberList.add(3);
+        numberList.add(2);
+        numberList.add(3);
+        System.out.println(numberList);
+        //set will take always same amount of time no mather how many elements
+       Set<Integer> numberSet= new HashSet<>(numberList); // added list and is using hashTable behind scene
+       numberSet.addAll(numberList);//list added-second way
+        System.out.println(numberSet);
+
+        Set<String> nameSet1= new TreeSet<>();//
+        nameSet1.add("Walter");
+        nameSet1.add("Violetta");
+        nameSet1.add("Ibrahim");
+        nameSet1.add("Musa");
+        nameSet1.add("Muhamed");
+        //nameSet1.add("Muhamed"); no dup /ascending order
+
+        System.out.println(nameSet1);
+
+        Set<String> names1 = new LinkedHashSet<>();//no dup /inserting order/almost fast as hashSet
+        names1.add("Walter");
+        names1.add("Violetta");
+        names1.add("Ibrahim");
+        names1.add("Musa");
+        names1.add("Muhamed");
+        System.out.println(names1);
+
+
+
 
 
 //        for (String name: names){
