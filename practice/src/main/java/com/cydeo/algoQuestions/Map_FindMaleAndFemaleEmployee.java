009cfecc9ca4d0209a5f4b6fe3e17bb715721883
Kadifa <Kudickadifa@gmail.com>
2023-04-03 06:47:01 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/algoQuestions/Map_FindMaleAndFemaleEmployee.java
@@ -0,0 +1,39 @@
+package com.cydeo.algoQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_FindMaleAndFemaleEmployee {
+    public static void main(String[] args) {
+
+        Map<String, String> employeeMap = new HashMap<>();
+        employeeMap.put("Sandy", "F");
+        employeeMap.put("Mina", "F");
+        employeeMap.put("Tina", "F");
+        employeeMap.put("Mujo", "M");
+        employeeMap.put("Ahmed", "M");
+        employeeMap.put("Safet", "M");
+        employeeMap.put("Ilfan", "M");
+        employeeMap.put("Nedin", "M");
+        employeeMap.put("Asima", "F");
+        employeeMap.put("Asim", "M");
+
+
+        //How many female and male employees in the map?
+
+        int countMaleEmployees = 0;
+        int countFemaleEmployees = 0;
+
+        for (String eachValue : employeeMap.values()){
+            if(eachValue.equals("F")){
+                countFemaleEmployees++;
+            }else{
+                countMaleEmployees++;
+
+            }
+        }
+        System.out.println("Male employees:" + countMaleEmployees);
+        System.out.println("Female employees:"+ countFemaleEmployees);
+
+    }
+}
