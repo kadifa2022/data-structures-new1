cf7227bc3a6253a8aaf0161917a28bf3143bc225
Kadifa <Kudickadifa@gmail.com>
2023-04-04 21:39:59 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/algoQuestions/Map_UniqueCharacters2.java
@@ -0,0 +1,20 @@
+package com.cydeo.algoQuestions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Map_UniqueCharacters2 {
+    public static void main(String[] args) {
+        String str = "aabbcddddeeefffgghh";
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for(String each: str.split("")){
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+                if(frequency == 1)
+                    map.put(each.charAt(0), frequency);
+
+        }
+        System.out.println(map);
+    }
+}
