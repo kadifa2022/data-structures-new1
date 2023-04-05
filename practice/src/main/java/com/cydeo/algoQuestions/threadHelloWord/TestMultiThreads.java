f68cff04990d08be353825989b11d96a4bf468b8
Kadifa <Kudickadifa@gmail.com>
2023-04-04 20:54:24 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/algoQuestions/threadHelloWord/TestMultiThreads.java
@@ -0,0 +1,18 @@
+package com.cydeo.algoQuestions.threadHelloWord;
+
+public class TestMultiThreads {
+    public static void main(String[] args) {
+
+        ThreadHelloWord thread1 = new ThreadHelloWord(1);
+        ThreadHelloWord thread2 = new ThreadHelloWord(2);
+        ThreadHelloWord thread3 = new ThreadHelloWord(3);
+        ThreadHelloWord thread4 = new ThreadHelloWord(4);
+        ThreadHelloWord thread5 = new ThreadHelloWord(5);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+    }
+}
