6b7eaed6505c4361351f92b104c54ef790825cf9
Kadifa <Kudickadifa@gmail.com>
2023-04-09 06:20:32 -0500

    practice

--- practice/src/main/java/com/cydeo/algoQuestions/leetCodeInteviewQuestions/ListNode.java
+++ practice/src/main/java/com/cydeo/algoQuestions/leetCodeInteviewQuestions/ListNode.java
@@ -2,6 +2,11 @@ package com.cydeo.algoQuestions.leetCodeInteviewQuestions;
 
 public class ListNode {
  ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
     int val;
     public ListNode(ListNode next) {
         this.next = next;
