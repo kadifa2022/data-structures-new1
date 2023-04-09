677ce492a26c79c4d132fa7702640e11b6c6d2b5
Kadifa <Kudickadifa@gmail.com>
2023-04-09 06:12:41 -0500

    practice

---
+++ practice/src/main/java/com/cydeo/algoQuestions/leetCodeInteviewQuestions/ListNode.java
@@ -0,0 +1,12 @@
+package com.cydeo.algoQuestions.leetCodeInteviewQuestions;
+
+public class ListNode {
+ ListNode next;
+    int val;
+    public ListNode(ListNode next) {
+        this.next = next;
+    }
+
+
+    }
+
