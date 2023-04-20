4b75f920ab4db2363950da9216cd8f0468d22785
Kadifa <Kudickadifa@gmail.com>
2023-04-19 20:45:59 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Constructor_Remove_If_Method/studentTask_constructor/StudentsGroup.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Constructor_Remove_If_Method/studentTask_constructor/StudentsGroup.java
@@ -1,24 +1,31 @@
 package com.cydeo.Constructor_Remove_If_Method.studentTask_constructor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class StudentsGroup {
+public class StudentsGroup {//StudentsGroup HAS A Student(relationship)
 
     public String  groupName;
     public int groupId;
                         // Student is data type // students are instance variable
-    public ArrayList<Student> students = new ArrayList<>();
+    public ArrayList<Student> students = new ArrayList<>();//instants variable
 
 
     public StudentsGroup(String groupName, int groupId) {// creates the StudentsGroup object by setting the groupName and GroupId
         this.groupName = groupName;
         this.groupId = groupId;
         //students = new ArrayList<>(): // size 0
+
     }
         //students = new ArrayList<>();// on beginning size is 0
         //this method does:Takes one student object, and adds it to the arraylist of students
         public void addStudent(Student student){// instance
         students.add(student);
+    }
+        // Takes one array of students, and add the array of the students to the arraylist to the students
+    public void addStudent(Student [] students){
+        this.students.addAll(Arrays.asList(students));
+
     }
     //This method does: takes name, age, gender, id of studentInfo and creates student object by using the given info, and add's the students object to the arrayList
             public void addStudent(String name, int age, char gender, String id){
