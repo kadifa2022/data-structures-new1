2c3b8f5ba03855e5e8007f5a9900512695b77204
Kadifa <Kudickadifa@gmail.com>
2023-04-21 20:27:43 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Day27_AcessModifier/Person/Person.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Day27_AcessModifier/Person/Person.java
@@ -40,4 +40,31 @@ public class Person {
         this(name, age, gender);
         this.language = language;
     }
+    static {//static block
+        planet = "Earth";
+        isHuman = true;
+        hasNose = true;
+        numberOfWings =0;
+        numberOfHead =1;
+    }
+    public static void printPlanetName(){
+        System.out.println("Planet name is: " + planet);
+    }
+    public void eat(String food){
+        System.out.println(name + "is eating " + food);
+    }
+    public void drink(String drink){
+        System.out.println(name + "is drinking " + drink);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", language='" + language + '\'' +
+                ", planet ='" + planet + '\'' +
+                '}';
+    }
 }
