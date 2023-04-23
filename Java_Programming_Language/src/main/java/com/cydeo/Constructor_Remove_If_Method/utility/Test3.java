0b6e61ce1ab5862ec85da277291ead213cd49003
Kadifa <Kudickadifa@gmail.com>
2023-04-23 16:40:08 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Constructor_Remove_If_Method/utility/Test3.java
@@ -0,0 +1,13 @@
+package com.cydeo.Constructor_Remove_If_Method.utility;
+import com.cydeo.Day28_OOP_concepts.inheritance_Intro.ProtectedAccessModifier;
+
+public class Test3 extends ProtectedAccessModifier{
+
+
+    public static void main(String[] args) {
+
+        System.out.println(ProtectedAccessModifier.a);//static variable a
+        ProtectedAccessModifier.methodA();//static method1
+        // void -> for void we don't put print statement
+    }
+}
