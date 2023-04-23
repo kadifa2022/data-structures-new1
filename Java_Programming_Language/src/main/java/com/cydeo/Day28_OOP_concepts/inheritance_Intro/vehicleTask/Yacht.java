21b0a09b3e68496dacb195350d1e9932f7e0adab
Kadifa <Kudickadifa@gmail.com>
2023-04-23 16:40:22 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/vehicleTask/Yacht.java
@@ -0,0 +1,8 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro.vehicleTask;
+
+public class Yacht extends Boat{
+
+    public Yacht(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+}
