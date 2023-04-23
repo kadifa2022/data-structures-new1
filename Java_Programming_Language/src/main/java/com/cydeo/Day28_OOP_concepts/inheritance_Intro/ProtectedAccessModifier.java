52efe40bb66b8833a9e517c99df3246d54b59d73
Kadifa <Kudickadifa@gmail.com>
2023-04-23 16:40:01 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/ProtectedAccessModifier.java
@@ -0,0 +1,9 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro;
+
+public class ProtectedAccessModifier {
+
+    protected static int a = 100;
+    protected static void methodA(){
+
+    }
+}
