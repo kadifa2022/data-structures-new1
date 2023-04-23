490701b7798a2b5be9f146c878c9a17c8e6ee224
Kadifa <Kudickadifa@gmail.com>
2023-04-23 08:21:42 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance/Tiger.java
+++
@@ -1,8 +0,0 @@
-package com.cydeo.Day28_OOP_concepts.inheritance;
-
-public class Tiger extends Animal{
-
-    public void hunt(){
-        System.out.println(name + " is hunting");
-    }
-}
---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/Tiger.java
@@ -0,0 +1,8 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro;
+
+public class Tiger extends Animal{
+
+    public void hunt(){
+        System.out.println(getName() + " is hunting");
+    }
+}
