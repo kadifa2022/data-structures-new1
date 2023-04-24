bf3907fcdc599b5f0119c3187c883ffadd54e34d
Kadifa <Kudickadifa@gmail.com>
2023-04-24 11:35:59 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/shape_methodOverriding/TestShapeObjects.java
@@ -0,0 +1,11 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro.shape_methodOverriding;
+
+public class TestShapeObjects {
+    public static void main(String[] args) {
+
+        Square square = new Square(5);
+        System.out.println(square);
+
+        square.drew();
+    }
+}
