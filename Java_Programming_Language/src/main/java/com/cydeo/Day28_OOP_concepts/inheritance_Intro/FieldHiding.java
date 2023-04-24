ce6416de4534247c5512af64a0e32668fbeb2aee
Kadifa <Kudickadifa@gmail.com>
2023-04-24 11:35:34 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/FieldHiding.java
@@ -0,0 +1,16 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro;
+
+class A{
+    public int a = 100;
+
+}
+
+
+public class FieldHiding extends A{
+    public int a = 300;
+
+
+    public static void main(String[] args) {
+        System.out.println(new FieldHiding().a);
+    }
+}
