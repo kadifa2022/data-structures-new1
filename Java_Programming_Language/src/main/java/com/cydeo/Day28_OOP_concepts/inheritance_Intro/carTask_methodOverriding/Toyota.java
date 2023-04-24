8809b6d575ed93b63de222bbeaf381194d6a233f
Kadifa <Kudickadifa@gmail.com>
2023-04-24 16:01:12 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/carTask_methodOverriding/Toyota.java
@@ -0,0 +1,10 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro.carTask_methodOverriding;
+
+public class Toyota extends Car{
+
+
+    public Toyota(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
+
+}
