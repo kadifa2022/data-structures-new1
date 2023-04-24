f557a407ec8cc4c6b96365ee78a49d9887acd29d
Kadifa <Kudickadifa@gmail.com>
2023-04-24 11:35:51 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/shape_methodOverriding/Square.java
@@ -0,0 +1,41 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro.shape_methodOverriding;
+
+
+public class Square extends Shape{ // no compile error because no parameter in constructor
+
+    private double side;
+    public Square(double side){
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public void drew() {
+        super.drew();
+        System.out.println("\t* * * * * *");
+        for(int i= 0; i<4;i++){
+            System.out.println("\t*         *");
+        }
+
+
+        System.out.println("\t* * * * * *");
+    }
+
+}
