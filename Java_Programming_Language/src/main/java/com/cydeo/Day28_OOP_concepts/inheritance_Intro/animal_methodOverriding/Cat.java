38119810189e50fd4572b34724b94250b891d45f
Kadifa <Kudickadifa@gmail.com>
2023-04-24 11:35:19 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/inheritance_Intro/animal_methodOverriding/Cat.java
@@ -0,0 +1,24 @@
+package com.cydeo.Day28_OOP_concepts.inheritance_Intro.animal_methodOverriding;
+
+public class Cat extends Animal{
+
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+    //static, private and final() can't be override
+    //only instant(), and same() from parent class-> access modifier must be the same or  more visible
+    @Override // if @ gives you compile error method is not override
+    public void eat(){//same method different implementation
+        System.out.println("Cat " + getName()+ "is eating cat food");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cat "+ getName()+ " sleeps 12 hours in a day");
+    }
+
+
+    public void scratch(){
+        System.out.println("Cat " + getName() + " is scratching");
+    }
+}
