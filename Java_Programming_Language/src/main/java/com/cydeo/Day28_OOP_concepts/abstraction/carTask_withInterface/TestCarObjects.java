15e1dcf29f4eb189f9018b288b3aac1dbf125393
Kadifa <Kudickadifa@gmail.com>
2023-04-26 10:24:13 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/carTask_withInterface/TestCarObjects.java
@@ -0,0 +1,28 @@
+package com.cydeo.Day28_OOP_concepts.abstraction.carTask_withInterface;
+
+public class TestCarObjects {
+
+    public static void main(String[] args) {
+
+       // Car ca1 = new Car("X6", "Red", 2020, 45000);abstract class and can't be created object from abstract class
+        Honda honda= new Honda("Pilot", "Red", 2020, 45000);
+        Audi audi = new Audi("Q6", "White", 2022, 50000);
+        Tesla tesla= new Tesla("Model Y", "Blue", 2022, 60000);
+
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
+        System.out.println(" ------------------------------     ");
+
+        honda.stop();
+        audi.stop();
+        tesla.stop();
+
+        System.out.println("----------------------------------------------");
+        honda.start();
+        audi.start();
+        tesla.start();
+    }
+
+
+}
