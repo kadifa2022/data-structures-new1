8ec6df3bb474f3696091627d0580465591674a81
Kadifa <Kudickadifa@gmail.com>
2023-04-26 22:14:45 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/animalTask_withInterface/Duck.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/animalTask_withInterface/Duck.java
@@ -1,6 +1,6 @@
 package com.cydeo.Day28_OOP_concepts.abstraction.animalTask_withInterface;
 
-public class Duck extends Animal{
+public class Duck extends Animal implements Playable, Flyable,Swimmable{
 
     public Duck(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -10,6 +10,24 @@ public class Duck extends Animal{
     public void eat() {
 
         System.out.println("Duck " + getName()+ " is eating grass");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Duck " +getName() + " is flaying ");
+
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Duck" + getName()+ " is playing ");
+
+    }
+
+    @Override
+    public void swim() {
+        System.out.println();
 
     }
 }
