21a459515d07716e63f16303652275f6a6f82dca
Kadifa <Kudickadifa@gmail.com>
2023-04-27 08:06:32 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/TransportationTask/AutoPark.java
@@ -0,0 +1,10 @@
+package com.cydeo.Day28_OOP_concepts.abstraction.TransportationTask;
+
+public interface AutoPark {
+
+    boolean hasAutoPark = true;// public , static are given by default
+
+     void autoPark();
+
+    }
+
---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/TransportationTask/AutoPilot.java
@@ -0,0 +1,8 @@
+package com.cydeo.Day28_OOP_concepts.abstraction.TransportationTask;
+
+public interface AutoPilot extends AutoPark {
+
+    void selfDrive();
+
+
+}
