4723eb9f163bc70fc60c470113042155f471a6fa
Kadifa <Kudickadifa@gmail.com>
2023-04-26 22:14:51 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/animalTask_withInterface/Eagle.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/animalTask_withInterface/Eagle.java
@@ -1,6 +1,6 @@
 package com.cydeo.Day28_OOP_concepts.abstraction.animalTask_withInterface;
 
-public class Eagle extends Animal{
+public class Eagle extends Animal implements Flyable,WildAnimal{
     public Eagle(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
@@ -9,6 +9,18 @@ public class Eagle extends Animal{
     public void eat() {
         System.out.println("Eagle" + getName()+ " is eating snake");
 
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Eagle " +getName()+ " is  flying");
+
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println();
 
     }
 }
---
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/abstraction/animalTask_withInterface/Flyable.java
@@ -0,0 +1,7 @@
+package com.cydeo.Day28_OOP_concepts.abstraction.animalTask_withInterface;
+
+public interface Flyable {
+    boolean canFly = true;
+
+    void fly();
+}
