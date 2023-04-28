14935d000f8d2da595dba28fd61ac9d00364549e
Kadifa <Kudickadifa@gmail.com>
2023-04-28 10:00:59 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/polymorphism/TestEqualsMethod.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Day28_OOP_concepts/polymorphism/TestEqualsMethod.java
@@ -1,5 +1,6 @@
 package com.cydeo.Day28_OOP_concepts.polymorphism;
 
+import com.cydeo.Day28_OOP_concepts.inheritance.phoneTask.IPhone;
 import com.cydeo.Day28_OOP_concepts.inheritance.shape_methodOverriding.Circle;
 
 public class TestEqualsMethod {
@@ -13,6 +14,16 @@ public class TestEqualsMethod {
         System.out.println(circle1 == circle2); //false// two different objects // in different memory heap location
         System.out.println(circle1.equals(circle2));//
         System.out.println(circle1.equals(circle3));
+
+
+        System.out.println("---------------------------------------------");
+
+       IPhone iPhone1 = new IPhone("Iphone12", "Large", 1000.5, "Black");
+       IPhone iPhone2 = new IPhone("Iphone12", "Large", 1000.5, "Black");
+
+        System.out.println(iPhone1.equals(iPhone2));
+
+
 
     }
 }
