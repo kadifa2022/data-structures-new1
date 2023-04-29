a499f70a874ec8df5c7c057947614d84b0eb7b2f
Kadifa <Kudickadifa@gmail.com>
2023-04-29 13:55:21 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/day37_exceptions/TryCatchBlocks2.java
+++ Java_Programming_Language/src/main/java/com/cydeo/day37_exceptions/TryCatchBlocks2.java
@@ -1,7 +1,10 @@
 package com.cydeo.day37_exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         System.out.println("Program1 started");
         int[] arr = {1, 2, 3, 4, 5};
         try {
@@ -19,10 +22,44 @@ public class TryCatchBlocks2 {
         try {
             System.out.println(9 / 0);
         } catch (RuntimeException e) {
-           // e.printStackTrace();// full details description of the
+            // e.printStackTrace();// full details description of the
             System.out.println(e.getMessage());
         }
         System.out.println("Program2 ended");
+
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("Program3 started");
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        }
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("Program4 started");
+        try{
+            Thread.sleep(3000);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Program4 ended");
+
+        System.out.println("-----------------------------------------");
+
+        //finally block -> optinal, always get executed->
+
+
+
+
+
+
+
+
 
     }
 }
