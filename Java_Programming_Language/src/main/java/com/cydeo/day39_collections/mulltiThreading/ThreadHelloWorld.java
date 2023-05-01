a365da8bf334cb2cdc90843e5e967ea8dcc6c9c3
Kadifa <Kudickadifa@gmail.com>
2023-05-01 12:05:33 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/day39_collections/mulltiThreading/ThreadHelloWorld.java
+++ Java_Programming_Language/src/main/java/com/cydeo/day39_collections/mulltiThreading/ThreadHelloWorld.java
@@ -2,11 +2,22 @@ package com.cydeo.day39_collections.mulltiThreading;
 
 public class ThreadHelloWorld extends Thread{// extending Thread class
 
+    int threadNumber;// created instances to see order of threat
 
+    public ThreadHelloWorld(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    //thread created in run()
     @Override
     public void run() {//right click and override methode run() from Threat class
         for(int i = 1; i< 6; i++) {// we can create multi threads n java
-            System.out.println("Hello world " + 1);
+            System.out.println("Hello world " + i+ " from Thread " + threadNumber);// iteration
+            try{
+                Thread.sleep(200);
+            }catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
         }
 
 
