a993f8702640bcda8b4b1dcbafc9c6a3f1e2320d
Kadifa <Kudickadifa@gmail.com>
2023-05-03 12:41:42 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/day42_maps/MapPractice2.java
+++ Java_Programming_Language/src/main/java/com/cydeo/day42_maps/MapPractice2.java
@@ -1,5 +1,7 @@
 package com.cydeo.day42_maps;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.time.LocalDate;
 import java.util.*;
 
@@ -13,7 +15,7 @@ public class MapPractice2 {
         person1.put("gender", 'M');
         person1.put("age", 32);
         person1.put("job_title", "Developer");
-        person1.put("salary", 1000000.5);
+        person1.put("salary", 100000);
         person1.put("hire_date", LocalDate.of(2012,1,15));
         person1.put("married", true);
 
@@ -26,7 +28,7 @@ public class MapPractice2 {
         person2.put("age", 31);
         person2.put("job_title", "Back-EndDeveloper");
         person2.put("salary", 90000);
-        person2.put("hire_date", LocalDate.of(2022,4,10));
+        person2.put("hire_date", LocalDate.of(2019,4,10));
         person2.put("married", true);
 
         System.out.println(person2.get("name"));
@@ -55,14 +57,13 @@ public class MapPractice2 {
 
 
 
-
         Map<String, Object> person5= new LinkedHashMap<>();
         person5.put("name", "Tina");
         person5.put("gender", 'F');
         person5.put("age", 19);
         person5.put("job_title", "Back-EndDeveloper");
         person5.put("salary", 90000);
-        person5.put("hire_date", LocalDate.of(2022,4,10));
+        person5.put("hire_date", LocalDate.of(2021,4,10));
         person5.put("married", true);
 
 
@@ -87,6 +88,74 @@ public class MapPractice2 {
 
             }
         }
+
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::;");
+
+        //display all names of the employee who are hired 2022
+
+        for(Map<String, Object> eachMap:listOfMap){//first iterate List of maps to get each employee
+            for (Map.Entry<String, Object> eachEntry : eachMap.entrySet()) {//to get eachEntry
+                if(eachEntry.getKey().equals("hire_date")){
+                    //local date is cast
+                    if( ((LocalDate)eachEntry.getValue() ).getYear() == 2022){
+                        System.out.println(eachMap.get("name"));//
+
+                    }
+
+                }
+            }
+        }
+
+        System.out.println("------------------------------------------------------------");
+        //to get first map
+        listOfMap.get(0).replace("name", "Vasyl");          //cast to integer(because is reference type object) and the add to salary +50000
+
+        listOfMap.get(listOfMap.size()-1).replace("salary", ((Integer) listOfMap.get(listOfMap.size()-1).get("salary")) + 50000);
+
+        listOfMap.get(1).replace("age", ((Integer)listOfMap.get(1).get("age")) + 4);
+
+        System.out.println(listOfMap);
+
+        System.out.println("-------------------------------------------------");
+        // to keep everything in order we are using LinkedHashMap
+
+        Map<String, Object> car1 = new LinkedHashMap<>();
+        car1.put("brand", "BMW");
+        car1.put("model", "X5");
+        car1.put("year", 2021);
+        car1.put("color","Red");
+        car1.put("price", 45000.5);
+
+
+        Map<String, Object> car2 = new LinkedHashMap<>();
+        car2.put("brand", "Mercedes");
+        car2.put("model", "X5");
+        car2.put("year", 2020);
+        car2.put("color","Blue");
+        car2.put("price", 55000.5);
+
+        Map<String, Object> car3 = new LinkedHashMap<>();
+        car3.put("brand", "Audi");
+        car3.put("model", "X5");
+        car3.put("year", 2020);
+        car3.put("color","White");
+        car3.put("price", 55000.5);
+
+        Map<String, Object> car4 = new LinkedHashMap<>();
+        car4.put("brand", "Honda ");
+        car4.put("model", "cvs");
+        car4.put("year", 2020);
+        car4.put("color","Black");
+        car4.put("price", 55000.5);
+
+        List<Map<String, Object>> carList = new ArrayList<>();
+
+        carList.addAll(Arrays.asList(car1, car2, car3, car4));
+
+
+
+
+
 
 
 
