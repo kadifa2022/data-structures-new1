907c1e683ed0b5ebe3c29fdbbc2df035c48b53ef
Kadifa <Kudickadifa@gmail.com>
2023-05-04 19:46:49 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/labs/lab06_Methods/StringUtility.java
+++ Java_Programming_Language/src/main/java/com/cydeo/labs/lab06_Methods/StringUtility.java
@@ -18,7 +18,8 @@ public class StringUtility {
 
  */
 // return the frequency of the given word from the given sentence
-    public static int frequencyOfSentenceAndWord(String sentence, String word){// solution 2
+    //frequencyOfSentenceAndWord
+    public static int frequency(String sentence, String word){// solution 2
 
         int count= 0;
         while(sentence.contains(word)){
@@ -27,5 +28,53 @@ public class StringUtility {
         }
         return count;
     }
+    //                          "aabacde"      'a' // to find frequency of one  character
+    public static int frequency(String str, char ch){// frequency of string and char
+        int count =0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) ==ch){
+                count ++;
+            }
+
+        }
+        return count;
+
+    }
+
+    public static String reverse(String word){
+
+        String result = "";
+
+        for (int i = word.length() -1; i >= 0;  i--) {
+            char each = word.charAt(i);
+            result +=each;
+
+        }
+        return result;
+
+    }
+
+    public static String removeDup(String str){
+        String result = "";
+
+        for (int i = 0; i < str.length(); i ++){
+            char each = str.charAt(i);
+            if(!result.contains(""+each)){
+                result += each;
+            }
+        }
+        return  result;
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
