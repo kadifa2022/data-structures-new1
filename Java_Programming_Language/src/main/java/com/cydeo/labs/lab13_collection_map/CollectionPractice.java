1035e246b44653cd41488b8d95854415cfcac08c
Kadifa <Kudickadifa@gmail.com>
2023-05-05 10:52:46 -0500

    practice

--- Java_Programming_Language/src/main/java/com/cydeo/labs/lab13_collection_map/CollectionPractice.java
+++ Java_Programming_Language/src/main/java/com/cydeo/labs/lab13_collection_map/CollectionPractice.java
@@ -48,11 +48,41 @@ public class CollectionPractice {
         employees.add("Muhamed");
         employees.add("Jusuf");
         employees.add("Daniel");
-        employees.add("James");
-        employees.add("James");
-        employees.add("James");
-        employees.add("James");
+//        employees.add("James");
+//        employees.add("James");
+//        employees.add("James");
+//        employees.add("James");
         System.out.println(employees);
+
+       // System.out.println(employees.get(0)); cat use get() no index number
+
+        for (String each : employees) {
+            System.out.println(each);}
+
+            // we can convert collection to another collection through constructor
+            System.out.println(new ArrayList<>(employees).get(1));// Set(employee) converted to ArrayList
+
+
+        System.out.println("-------------Iterable -removeIf() -----------------------");// removeIf () using if we have condition like(remove all words that start with "J")
+
+        List<String> words = new ArrayList<>();
+        words.addAll(Arrays.asList(
+                "Java", "Java", "JAVA", "Python", "C#", "C++","Ruby","Jva", "Jamila"));
+        words.removeIf(p->p.toLowerCase().startsWith("j") && p.toLowerCase().endsWith("a"));
+        System.out.println(words);
+        // all collection has forEach() and takes consumer takes ona argument ad don't return any value
+        // we use lambda expression just for overriding abstract method of functional interface
+        words.forEach(c->{//forEach() added with Java 8
+            if(!c.equals("Python"))
+            System.out.println(c);
+        });
+
+
+
+
+
+
+
 
 
 
