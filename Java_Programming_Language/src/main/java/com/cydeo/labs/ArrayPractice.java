60d3b4ba6813d48d6bbfaf48c23b40113be87889
Kadifa <Kudickadifa@gmail.com>
2023-05-04 22:36:18 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/labs/ArrayPractice.java
@@ -0,0 +1,48 @@
+package com.cydeo.labs;
+
+
+
+
+
+
+
+
+
+import java.util.Arrays;
+
+
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[5];// one way to initial Array
+        nums[0] =10;
+        nums[1] =20;
+        nums[2] =30;
+        nums[3] =40;
+        nums[4] =50;
+        nums = new int[6];
+        System.out.println(Arrays.toString(nums));
+
+
+        System.out.println("--------------------------------------------");
+
+        int[]scores = {10,20,30,40,50}; //second way to initial Array
+        //int[]scores2 = new int []{10,20,30,40,50};
+
+        System.out.println("----------------------------------------------");
+        int[][] arr2D = {};//for 1-D Array
+        int[][][] arr3D ={};// for 2-D Array
+
+        System.out.println("-------------------------------------------------");
+
+
+        }
+
+
+
+    }
+
+
+
