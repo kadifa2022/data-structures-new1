bea31650d51a698a12d9f40d1e477f3171805a07
Kadifa <Kudickadifa@gmail.com>
2023-05-09 18:39:18 -0500

    practice

---
+++ dsa-04-LinkedLists/src/main/java/linkedListTask/Contact.java
@@ -0,0 +1,17 @@
+package linkedListTask;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contact {
+
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
