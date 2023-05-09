7097234b5dfc839aa870d9c3df70f99739048618
Kadifa <Kudickadifa@gmail.com>
2023-05-09 18:39:25 -0500

    practice

---
+++ dsa-04-LinkedLists/src/main/java/linkedListTask/Main.java
@@ -0,0 +1,17 @@
+package linkedListTask;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is our linked list task as group 3");
+
+        PhoneBookLinkedList p = new PhoneBookLinkedList();
+
+        Contact contact1 = new Contact("Kevin", "Li", "kevin@gmail.com", "234567");
+        Contact contact2 = new Contact("Mike", "Smith", "mike@gmail.com", "9876532");
+        Contact contact3 = new Contact("Yasemin", "Smith", "yasemin@gmail.com", "4567890");
+
+
+
+    }
+}
