9175eb66edf9c1ff509199bb391469b923a87faf
Kadifa <Kudickadifa@gmail.com>
2023-05-09 18:39:32 -0500

    practice

---
+++ dsa-04-LinkedLists/src/main/java/linkedListTask/PhoneBookLinkedList.java
@@ -0,0 +1,17 @@
+package linkedListTask;
+
+public class PhoneBookLinkedList {
+
+    public PhoneNode head;
+    public PhoneNode tail;
+    public int size;
+
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+        public int size(){
+            return size;
+        }
+    }
+
