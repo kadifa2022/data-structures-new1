ef8870da396dda5cf8f1407b7c97b4db5d472088
Kadifa <Kudickadifa@gmail.com>
2023-05-25 21:20:26 -0500

    practice swap two numbers

---
+++ practice/src/main/java/com/cydeo/javaCore/SwapTwoNumbers.java
@@ -0,0 +1,28 @@
+package com.cydeo.javaCore;
+
+public class SwapTwoNumbers {
+
+
+    public static void main(String[] args) {
+        swap1(3, 5);
+
+        swap2(5,6);
+    }
+    //solution 1
+    public static void swap1(int a, int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    //solution 2
+
+    public static void swap2(int a, int b){
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
