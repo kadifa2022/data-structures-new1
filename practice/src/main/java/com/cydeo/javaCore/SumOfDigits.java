91a3033b2e26c8bd4f87270211ffa0f52902e967
Kadifa <Kudickadifa@gmail.com>
2023-05-25 21:31:27 -0500

    practice swap two numbers

---
+++ practice/src/main/java/com/cydeo/javaCore/SumOfDigits.java
@@ -0,0 +1,25 @@
+package com.cydeo.javaCore;
+
+public class SumOfDigits {
+    /*
+    Write a function that can return the sum of digits of an integer Do not use any String manipulation
+    ex:
+    input : 123
+    output : 6
+     */
+
+    public static int sumOfDigits(int number){
+        int sum = 0;
+        while (number > 0){
+            sum +=(number % 10);
+            number /=10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(1231));
+        System.out.println(20 + 20);
+    }
+
+}
