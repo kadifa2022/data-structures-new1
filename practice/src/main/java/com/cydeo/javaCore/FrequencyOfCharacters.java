16deddff533b0a67abba65d6201485a56f47efd7
Kadifa <Kudickadifa@gmail.com>
2023-05-26 22:01:51 -0500

    practice swap two numbers

---
+++ practice/src/main/java/com/cydeo/javaCore/FrequencyOfCharacters.java
@@ -0,0 +1,26 @@
+package com.cydeo.javaCore;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+
+
+
+
+    public static void frequencyTest(String str){
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for(Character each : str.toCharArray()){
+            if(map.containsKey(each)){
+                map.put(each, map.get(each) + 1);
+            }else{
+                map.put(each, 1);
+            }
+        }
+        System.out.println();
+    }
+}
+/*
+Write a method that prints the frequency of each character
+ */
+
