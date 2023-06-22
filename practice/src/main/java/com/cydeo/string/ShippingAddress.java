ebe27a11c0c84fa36b6354dcb178ab1635111b30
Kadifa <Kudickadifa@gmail.com>
2023-06-22 18:57:13 -0500

    practice frequency of each element

--- practice/src/main/java/com/cydeo/string/ShippingAddress.java
+++ practice/src/main/java/com/cydeo/string/ShippingAddress.java
@@ -16,18 +16,35 @@ public class ShippingAddress {
         System.out.println("Enter your building number:");
         String building_number = input.next();// we assume will be only one word
         // if i use nextLine() after next() a have to add additional nextLine() to clean scanner
-        input.nextLine(); // to clear the scanner
+        input.nextLine(); // to clear the scanner additional method
 
         System.out.println("Enter your Street name");
         String streetName = input.nextLine();
 
         System.out.println("Enter your city name");
+        String cityName = input.nextLine();
+
         System.out.println("Enter your state");
+        String stateName = input.nextLine();
+
         System.out.println("Enter your zipCode");
+        String zipcode = input.next();
 
 
         System.out.println("full_name" + full_name);
         System.out.println("building_number "  + building_number);
+        System.out.println("Street name " + streetName);
+        System.out.println("City name" + cityName);
+        System.out.println("State " + stateName );
+        System.out.println("ZipCode" + zipcode);
+
+        //----------------------------------------------
+        System.out.println("Your shipping address is:");
+        System.out.println("\t" + full_name);
+        System.out.println("\t" + building_number+" " + streetName);
+        System.out.println("\t" + cityName + ", " + stateName + " " +zipcode );
+
+        input.close();
 
     }
 
@@ -41,5 +58,12 @@ public class ShippingAddress {
         1.4 Enter your city name (nextLine())
         1.5 Enter your state (nextLine())
         1.6 Enter your zipCode (next())
+
+        1.7 Display the shipping address
+        Ex:
+        Your shipping address is:
+        John Smith
+        7925 Jones Branch Dr
+        McLean, VA 22012
 
  */
