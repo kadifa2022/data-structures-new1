6d1a602db7d7bcdb31bbbc9585954ae6c7284f4f
Kadifa <Kudickadifa@gmail.com>
2023-06-23 15:01:09 -0500

    practice big O notation

---
+++ dsa-01-AlgortihmAnalysis/src/main/java/Practice/BigOSingleLoop.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class BigOSingleLoop {
+
+    public static void main(String[] args) {
+        long numberOfOperations = 0;
+        int n = 10000;// size of  Data
+        int m = 500;
+        long startTime;
+        long endTime;
+        System.out.println("Input size n is = " + n);
+
+
+        // Task 1
+        startTime=System.currentTimeMillis();
+
+        for (int i = 0; i < n ; i++) {
+            numberOfOperations+=1;
+        }
+        endTime=System.currentTimeMillis();
+    }
+}
