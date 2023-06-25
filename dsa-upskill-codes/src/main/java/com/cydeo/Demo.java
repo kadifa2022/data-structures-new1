5b67f7b164644de550e4724d0108a4668ebb7e3b
Kadifa <Kudickadifa@gmail.com>
2023-06-24 23:03:49 -0500

    practice big O notation

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/Demo.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+    public static void main(String[] args) {
+       // Car car = new Car(2314,"Audi");// string is keeping in string pool
+        //System.out.println(car);
+
+        List<Integer> numbers = new ArrayList<>();
+    }
+}
