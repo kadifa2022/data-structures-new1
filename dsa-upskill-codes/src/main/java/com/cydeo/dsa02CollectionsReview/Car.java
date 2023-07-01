7a6fa7cf242f9a2bc0fa01ae32753f36c8ab5033
Kadifa <Kudickadifa@gmail.com>
2023-07-01 15:55:57 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa02CollectionsReview/Car.java
@@ -0,0 +1,15 @@
+package com.cydeo.dsa02CollectionsReview;
+
+public class Car {
+    String  name;
+    int id;
+    Car nextCar;// object instance of the class
+
+    Computer computer;//object instance of the class carr
+
+
+    public Car(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
