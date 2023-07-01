10c14f33ce7848d98131042cb86240628976eb0e
Kadifa <Kudickadifa@gmail.com>
2023-07-01 15:56:06 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa02CollectionsReview/Computer.java
@@ -0,0 +1,13 @@
+package com.cydeo.dsa02CollectionsReview;
+
+public class Computer {
+    int id;
+    String name;
+
+    public Computer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
