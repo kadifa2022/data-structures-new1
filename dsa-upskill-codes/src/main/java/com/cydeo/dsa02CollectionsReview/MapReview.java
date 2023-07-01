b18bb8e5ef397ae10adb00a02a5cf206fadf730e
Kadifa <Kudickadifa@gmail.com>
2023-07-01 15:56:40 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa02CollectionsReview/MapReview.java
@@ -0,0 +1,19 @@
+package com.cydeo.dsa02CollectionsReview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> studentsMap = new HashMap<>();
+        studentsMap.put(1,"Jack");
+        studentsMap.put(2, "Julia");
+        studentsMap.put(3,"Alison");
+
+        //How to get entrySet()
+
+        //How to get keySet()
+    }
+}
