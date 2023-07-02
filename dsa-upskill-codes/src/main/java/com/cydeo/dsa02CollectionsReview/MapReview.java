a7964a9ab1832f19bca672ada5a153e78b730315
Kadifa <Kudickadifa@gmail.com>
2023-07-02 15:32:29 -0500

    practice Set review

--- dsa-upskill-codes/src/main/java/com/cydeo/dsa02CollectionsReview/MapReview.java
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa02CollectionsReview/MapReview.java
@@ -1,7 +1,10 @@
 package com.cydeo.dsa02CollectionsReview;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapReview {
 
@@ -12,8 +15,36 @@ public class MapReview {
         studentsMap.put(2, "Julia");
         studentsMap.put(3,"Alison");
 
-        //How to get entrySet()
+        String str = "jjAVAdEVELOPER";
+        System.out.println(findFirstNonRepeating(str));
 
-        //How to get keySet()
+
     }
+
+    public static Character findFirstNonRepeating(String str){
+
+        //create map: will cost me space complexity using another ds O(N)
+        Map<Character, Integer> map = new TreeMap<>();
+        int count;//FREQUENCY
+        //count the frequency of chars(for each loop)
+        for (Character ch:str.toCharArray()) {//cont characters/ turning in to char array
+            if(map.containsKey(ch)){// if the map   character is not in the list
+                count = map.get(ch);// getting characters and adding to the list
+                map.put(ch, count +1);// increasing by 1 and put in the map key is the character value us count
+            }
+            else map.put(ch,1);
+        }
+        
+        //start from the beginning and check if there is a char frequency 1
+        for (Character ch:str.toCharArray()) {
+            if(map.get(ch)==1) return ch;
+            
+        }
+
+
+
+        return null;
+    }
+
+
 }
