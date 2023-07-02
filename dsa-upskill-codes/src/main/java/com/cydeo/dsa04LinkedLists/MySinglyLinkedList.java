b1275c604e7a7ad11c5b0c93db03a92119862155
Kadifa <Kudickadifa@gmail.com>
2023-07-02 13:25:32 -0500

    practice Set review

--- dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/MySinglyLinkedList.java
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/MySinglyLinkedList.java
@@ -24,93 +24,218 @@ public class MySinglyLinkedList {
 
     }
 
-    public int getKthItemFromLast(int k){
+    public int getKthItemFromLast(int k) {
         // create 2 pointers
         Node ptr1 = head;
         Node ptr2 = head;
 
         //move ptr2 k-1  times
-        for (int i = 0; i<k-1; i++){
+        for (int i = 0; i < k - 1; i++) {
             ptr2 = ptr2.next;
         }
         //move both pointers until ptr2 hits the last element
-        while(ptr2.next != null){
-            ptr1= ptr1.next;
-            ptr2= ptr2.next;
+        while (ptr2.next != null) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
         }
         //ptr1 is on the kth element from the last
         return ptr1.id;
     }
 
-    public void removeKthItemFromLast(int k){
+    public void removeKthItemFromLast(int k) {
         //create 3 pointers
         Node ptr1 = head;
         Node ptr2 = head;
         Node prev = null;
         //move pth2 k-1 times
-        for (int i = 0; i < k-1; i++) {
+        for (int i = 0; i < k - 1; i++) {
             ptr2 = ptr2.next;
         }
         //move both pointers until prt2 hits the last element
-        while(ptr2.next != null){
-            prev=ptr1;
-            ptr1=ptr1.next;
-            ptr2= ptr2.next;
+        while (ptr2.next != null) {
+            prev = ptr1;
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
         }
         //ptr1 is on the kth element from the last
         //do delete operation
-        if(ptr1==head){
-            head=ptr1.next;
-            ptr1.next= null;
+        if (ptr1 == head) {
+            head = ptr1.next;
+            ptr1.next = null;
             size--;
-        }else if(ptr1==tail){
-            tail= prev;
-            prev.next=null;
+        } else if (ptr1 == tail) {
+            tail = prev;
+            prev.next = null;
             size--;
-        }else{
-            prev.next=ptr1.next;
-            ptr1.next=null;
+        } else {
+            prev.next = ptr1.next;
+            ptr1.next = null;
             size--;
         }
 
     }
 
-    void add(int data){
+    void add(int data) {
         // create a new node object from data
         Node node = new Node(data);
         if (isEmpty()) {// if the list is empty
             head = tail = node;
             size++;
-        }else{//if there are elements in  list
-            tail.next= node;
+        } else {//if there are elements in  list
+            tail.next = node;
             tail = node;
             size++;
         }
     }
 
+    void deleteById(int id) {
+        //check if empty
+        if (isEmpty()) ;
+        System.out.println("List is empty!!");
+
+        // assign prev and current with head
+        Node prev = head;
+        Node current = head;
+        while (current != null) {
+            if (current.id == id) {//there is a match
+                //case 1: head
+                if (current == head) {
+                    head = current.next;
+                    current.next = null;
+                }
+                //case 2: tail
+                else if (current == tail) {
+                    tail = prev;
+                    prev.next = null;// ex-tail will be eligible for Garbage collection
+                }
+                //case 3: middle
+                else {
+                    prev.next = current.next;
+                    current.next = null;// current will be for garbage collection
+                }
+                //after deletion
+                size--;
+            }
+            //move forward on the elements of the list
+            prev = current;
+            current = current.next;
+        }
+
+    }
+
+    int indexOf(int id) {
+        if (isEmpty()) return -1;
+        int pos = 0;
+        //iterate through yhe list
+        Node current = head;// set my current with starting element
+        while (current != null) {
+            if (current.id == id) return pos;
+            pos++;
+            current = current.next;
+        }
+        return -1;
+    }
 
 
+    void printNodes() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null) System.out.println(current.id + "=> null");
+            else {
+                System.out.println(current.id + "=>");
+            }
+            current = current.next;
+        }
+    }
+
+    public void removeKthFromLast2(int k) {
+        Node ptr1 = head;
+        Node ptr2 = head;
+        for (int i = 0; i <k-1 ; i++) {
+            ptr2=ptr2.next;
+
+            if(ptr2==null) System.out.println("Less than k elements");
+
+            else if(ptr2.next== null){
+                head=ptr1.next;
+                ptr1.next=null;
+                return;
+            }
+
+        }
+        while (ptr2.next.next!=null){
+            ptr1=ptr1.next;
+            ptr2=ptr2.next;
+        }
+        ptr1.next=ptr1.next.next;
+        ptr1=ptr1.next;
+        ptr1= null;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
