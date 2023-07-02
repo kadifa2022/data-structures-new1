81826628d4cf0f2f7f10486f67937f35b34396d7
Kadifa <Kudickadifa@gmail.com>
2023-07-01 22:01:29 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/Car.java
@@ -0,0 +1,11 @@
+package com.cydeo.dsa04LinkedLists;
+
+public class Car {
+    String brand;
+    String color;
+
+    public Car(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
+    }
+}
---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/MergeTwoLinkedList.java
@@ -0,0 +1,29 @@
+package com.cydeo.dsa04LinkedLists;
+
+import com.cydeo.dsa04LinkedLists.Node;
+
+public class MergeTwoLinkedList {
+
+    public static Node MergeLinkedLists(Node headOne, Node headTwo){
+        Node p1 = headOne;
+        Node p1Prev= null;
+        Node p2 = headTwo;
+
+        while (p1!= null && p2 != null){
+           if(p1.id<p2.id){
+               p1Prev = p1;
+               p1=p1.next;
+
+            }else{
+               if(p1Prev!=null) p1Prev.next= p2;
+               p1Prev =p2;
+               p2= p2.next;
+               p1Prev.next =p1;
+           }
+        }
+        if(p1 ==null) p1Prev.next =p2;
+        return headOne.id< headTwo.id ? headOne:headTwo;
+
+    }
+
+}
