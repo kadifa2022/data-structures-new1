563ec4916c95e9d25e88db80f5bde7a16489a836
Kadifa <Kudickadifa@gmail.com>
2023-07-03 17:29:23 -0500

    practice Set review

--- dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/SinglyLinkedListApp.java
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/SinglyLinkedListApp.java
@@ -10,6 +10,14 @@ public class SinglyLinkedListApp {
         myList1.add(1);myList1.add(3);myList1.add(6);myList1.add(7);
         myList2.add(2);myList2.add(4);myList2.add(5);
 
+        for (int i = 0; i<10; i++){
+            myList1.add(i);
+        }
+        myList1.printNodes();
+        myList1.deleteById(3);
+
+        System.out.println("index of 3" + myList1.indexOf(3));
+
         Node newHead= MergeTwoLinkedList.mergeLinkedLists(myList1.head, myList2.head);
 
 
