dd2ae143674374bf73bbaf71f1280a385e6b7c0b
Kadifa <Kudickadifa@gmail.com>
2023-07-03 19:31:43 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/singlyLinkedList/Node.java
@@ -0,0 +1,11 @@
+package com.cydeo.dsa04LinkedLists.singlyLinkedList;
+
+public class Node {
+    int id;
+    Node next;
+   // int size;
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
