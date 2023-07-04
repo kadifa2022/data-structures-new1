6c328a58ed5f6463568fc3f656faafb01c3d8b46
Kadifa <Kudickadifa@gmail.com>
2023-07-03 19:31:49 -0500

    practice Set review

---
+++ dsa-upskill-codes/src/main/java/com/cydeo/dsa04LinkedLists/singlyLinkedList/SinglyLinkedListApp.java
@@ -0,0 +1,17 @@
+package com.cydeo.dsa04LinkedLists.singlyLinkedList;
+
+public class SinglyLinkedListApp {
+
+    public static void main(String[] args) {
+
+        MySinglyLinkedList myList= new MySinglyLinkedList();
+        System.out.println(myList.isEmpty());
+
+        for (int i = 0; i <10 ; i++) {
+            myList.add(i);
+
+        }
+        myList.printNodes();
+
+    }
+}
