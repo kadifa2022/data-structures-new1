0913cb3ad86a0af4c48bab54116522354c25151e
Kadifa <Kudickadifa@gmail.com>
2023-07-05 19:04:40 -0500

    practice Set review

---
+++ java-labs/src/main/java/co/cydeo/lab1/UserRegister.java
@@ -0,0 +1,28 @@
+package co.cydeo.lab1;
+
+public class UserRegister {
+
+    public static void main(String[] args) {
+
+        String firstName = "Mike";
+        String lastName = "Smith";
+
+        String monthOfBirth = "May";
+        int dateOfBirt =10;
+        int yearOfBirth = 1980;
+        boolean isHealthPlanId = true;
+        boolean isSSNumber = false;
+
+        int memberId = 12345;
+        long policyNumber =12345884844L;
+        int last6DigitOfSsn = 100000;
+        String zipCode = "14256";
+
+        System.out.println(firstName +" who born in " + monthOfBirth + " wants health insurance from United with policy number "+ policyNumber);
+
+
+
+
+
+    }
+}
