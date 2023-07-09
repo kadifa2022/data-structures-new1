9e534c08e1a41098c02af79f98edb274720c2acf
Kadifa <Kudickadifa@gmail.com>
2023-07-09 12:50:39 -0500

    practice Set review

--- java-labs/src/main/java/co/cydeo/lab3pizzaApp1_Loops_Methods/PizzaApp.java
+++ java-labs/src/main/java/co/cydeo/lab3pizzaApp1_Loops_Methods/PizzaApp.java
@@ -2,12 +2,76 @@ package co.cydeo.lab3pizzaApp1_Loops_Methods;
 
 import java.util.Scanner;
 
-public class Task {
+public class PizzaApp {
     public static void main(String[] args) {
 
 
         System.out.println("Welcome to Best Pizza in Town");
         System.out.println("Please log in before placing the order");
+        String email = "mike@gmail.com";
+
+        makeOrder(login());
+
+    }
+
+    public static void makeOrder(String email) {
+        for (int i = 0; i < 1; ) {
+
+            System.out.println("Which pizza would you like to have. (Please type pizza  name. To create your own pizza, please type special)");
+
+            Scanner scanner = new Scanner(System.in);
+            String pizzaChoice = scanner.nextLine();
+
+
+            // selection statements -> in the system we have 3 kinds of pizza, Salami, Marg, Meat Lover,Special
+            if (checkPizzaThatWeHave(pizzaChoice)) {// valid  pizza
+                if (pizzaChoice.equals("special")) {//special pizza
+                    // build your pizza
+                    System.out.println("Building my own pizza");
+
+                } else {// ready pizza in the system
+                    System.out.println("We have received your order. Price is $19.99 ");
+
+                }
+                System.out.println("Your order number is " + generateOrderNumber(email));
+                break;
+
+            } else {
+                System.out.println("You have entered invalid pizza name. Please try again");
+            }
+        }
+
+    }
+
+    private static String generateOrderNumber(String email) {// order_m0i1k2e3@gmail.com
+
+        String orderNumber = "order_";
+        for (int i = 0; email.length() > i + 1; i++) {
+            orderNumber += email.substring(i, i + 1); //mike@gmail.com->m0i1k2e3@gmail.com
+            orderNumber += i;
+        }
+        return orderNumber;
+
+
+    }
+
+    private static boolean checkPizzaThatWeHave(String pizzaChoice) {
+        String pizza1 = "Margarita";
+        String pizza2 = "Salami";
+        String pizza3 = "Veggie";
+        String pizza4 = "fourCheese";
+        String pizzaSpecial = "Special";
+
+        return pizzaChoice.equals(pizza1) ||
+                pizzaChoice.equals(pizza2) ||
+                pizzaChoice.equals(pizza3) ||
+                pizzaChoice.equals(pizza4) ||
+                pizzaChoice.equals(pizzaSpecial);
+
+
+    }
+
+    public static String login() {
 
         // create 3 variables named email(String), account number(int), password and assign values
         String email = "mike@gmail.com";
@@ -28,7 +92,7 @@ public class Task {
                     System.out.println("Enter your email: ");
                     String givenEmail = scanner.next();
 
-                    if (!isEmailValid(givenEmail)) {
+                    if (!isInputValid(givenEmail)) {
                         System.out.println("Invalid Email. Please try again..");
                         continue;
                     }
@@ -59,10 +123,10 @@ public class Task {
                 case 2:
                     System.out.println("Please type your account number");
                     int an = scanner.nextInt();
-                    boolean checkAccountNo = an >= 100 && an < 1000;
+
 
                     // invalid and try again
-                    if (!checkAccountNo) {
+                    if (!isInputValid(an)) {
                         System.out.println("Invalid Account Number. Try again");
                         continue;
 
@@ -96,10 +160,15 @@ public class Task {
             break;// to stop for loop
 
         }
+        return email;
     }
 
+    private static boolean isInputValid(int an) {
+        boolean checkAccountNo = an >= 100 && an < 1000;
+        return checkAccountNo;
+    }
 
-    public static boolean isEmailValid(String givenEmail) {
+    public static boolean isInputValid(String givenEmail) {
 
 
         //Check if email address is valid// if@.available, and @ is before .
@@ -114,6 +183,7 @@ public class Task {
         }
         return isEmailDomainValid(givenEmail);
     }
+
     public static boolean isEmailDomainValid(String givenEmail) {
 
 
@@ -121,11 +191,11 @@ public class Task {
         String domain = givenEmail.substring(givenEmail.indexOf('@') + 1, givenEmail.indexOf('.'));
         boolean checkDomain = domain.equalsIgnoreCase("gmail") || domain.equalsIgnoreCase("hotmail");
 
-      return checkDomain;
+        return checkDomain;
     }
 
 
-    }
+}
 
 
 
