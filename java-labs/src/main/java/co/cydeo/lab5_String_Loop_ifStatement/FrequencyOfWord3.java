8ffcc55559772e76680c72fb80cdd52bf9b8508b
Kadifa <Kudickadifa@gmail.com>
2023-07-10 20:39:44 -0500

    practice Set review

--- java-labs/src/main/java/co/cydeo/lab5_String_Loop_ifStatement/FrequencyOfWord3.java
+++ java-labs/src/main/java/co/cydeo/lab5_String_Loop_ifStatement/FrequencyOfWord3.java
@@ -9,9 +9,10 @@ public class FrequencyOfWord3 {
         int countJava = 0; // assuming 0 "Java" in sentence
         // loop to make sure that start from beginning to the end of the sentence
         for (int i = 0; i <=sentence.length()-4; i++) {// -4 to avoid indexAutoBanException
-            // creating each possible string with 4 characters
+
             // creating substring from sentence
-            String each = sentence.substring(i, i+4);// in order to create substring always last index is excluded always add one more number
+            String each = sentence.substring(i, i+4);// creating all  possible string with 4 characters
+            // in order to create substring always last index is excluded always add one more number
 
             if(each.equals("Java")){ //if the substring equals to Java than count will be increased by 1
                 countJava++; // inc
@@ -20,5 +21,35 @@ public class FrequencyOfWord3 {
         }
         System.out.println(countJava);
 
+
+        System.out.println("=================================");
+
+        // frequency of cat
+
+        String  s = "Cat Cat Cat Cat Dog Dog Dog";
+
+        int countCat = 0;//assuming 0 cat in sentence
+        int countDog = 0;
+
+        for (int i =0; i <=s.length()-3; i++){
+
+            String each= s.substring(i, i+3);//creating all possible substring with 3 characters
+
+            if(each.equals("Cat")){// if the substring is equal to"cat"
+                countCat++;
+            }
+            if(each.equals("Dog")){
+                countDog ++;
+
+            }
+        }
+        System.out.println("CountCat " + countCat);
+        System.out.println("CountDog " + countDog);
+
+
     }
+
+
+
+
 }
