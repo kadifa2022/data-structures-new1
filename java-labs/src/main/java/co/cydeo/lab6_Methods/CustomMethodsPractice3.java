acb41ab0bf37bb595e740830b130fc53a330aaa6
Kadifa <Kudickadifa@gmail.com>
2023-07-11 18:30:21 -0500

    practice Set review

--- java-labs/src/main/java/co/cydeo/lab6_Methods/CustomMethodsPractice3.java
+++ java-labs/src/main/java/co/cydeo/lab6_Methods/CustomMethodsPractice3.java
@@ -7,19 +7,21 @@ public class CustomMethodsPractice3 {
         System.out.println(doubleChar("Cydeo"));
 
         System.out.println(endsLy("finally"));
+        System.out.println(hasBad("xxbad"));
+        System.out.println(atFirst("a1"));
+
+        System.out.println(isPalindrome("Cydeo"));
 
     }
 
 
-
-
     //The
-     public static String doubleChar(String str) {
+    public static String doubleChar(String str) {
 
 
         String result = "";// store result-> "TThhee"
 
-        for (int i = 0; i < str.length() ; i++) {
+        for (int i = 0; i < str.length(); i++) {
 
             char each = str.charAt(i);//i have all characters from giving string
 
@@ -29,13 +31,48 @@ public class CustomMethodsPractice3 {
             result += each;
         }
 
-      return result;
+        return result;
     }
 
-    public static boolean endsLy(String str){
+    public static boolean endsLy(String str) {
         return str.endsWith("ly");
     }
 
+    public static boolean hasBad(String str) {
+
+        if (str.equals("bad")) {//if the string (word) equals
+            return true;
+        }
+        if (str.length() < 4) {//if the length of the string does not have enough characters
+            return false;
+        }
+        if (str.substring(0, 3).equals("bad")) {
+            return true;
+        }
+        if (str.substring(1, 4).equals("bad")) {
+            return true;
+        }
+
+        return false;// otherwise false
+    }
+
+    public static String atFirst(String str) {
+
+        if (str.isEmpty()) {
+            return "@@";
+        } else if (str.length() == 1) {
+            return str + "@";
+        } else {
+            return str.substring(0, 2);
+        }
+
+    }
+
+    public static boolean isPalindrome(String str) {
+
+       return  StringUtility.reverse(str).equalsIgnoreCase(str);
+
+    }
 
 
 
