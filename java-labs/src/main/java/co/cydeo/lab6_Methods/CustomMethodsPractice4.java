28e8ad44f6461bf6b8f64452da3561da19812c15
Kadifa <Kudickadifa@gmail.com>
2023-07-11 20:35:48 -0500

    practice Set review

---
+++ java-labs/src/main/java/co/cydeo/lab6_Methods/CustomMethodsPractice4.java
@@ -0,0 +1,46 @@
+package co.cydeo.lab6_Methods;
+
+public class CustomMethodsPractice4 {
+
+    public static void main(String[] args) {
+        String str = "aaabccdeeeef";// find unique
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            /*
+            char each = str.charAt(i);
+           int f =  StringUtility.frequency(str, each);
+
+           if(f==1){
+               unique +=each;
+           }
+
+        }
+
+             */
+            if (StringUtility.frequency(str, str.charAt(i)) == 1) {//solution 2
+                unique += str.charAt(i);
+            }
+        }
+
+        System.out.println(unique);//bdf
+
+
+
+        System.out.println("-----------------------------");
+
+        String s = "aaaabbc";// "a4b2c1   // find the frequency of every single element
+        String nonDup = StringUtility.removeDup(s);
+
+        String result="";
+
+        for (int i = 0; i < nonDup.length() ; i++) {
+            char ch = nonDup.charAt(i);
+            int f = StringUtility.frequency(s, ch);
+            result += ""+ ch + f;
+        }
+        System.out.println(result);
+
+    }
+
+}
