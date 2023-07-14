f5ffc9b6706231dc1189e9965e95595f18e8f1f1
Kadifa <Kudickadifa@gmail.com>
2023-07-13 21:53:45 -0500

    practice budget splitting app

--- java-labs/src/main/java/co/cydeo/lab8_Budget_Spliter_App_Part1/Main.java
+++ java-labs/src/main/java/co/cydeo/lab8_Budget_Spliter_App_Part1/Main.java
@@ -12,7 +12,8 @@ public class Main {
         System.out.println("Welcome to Budget Planer!");
         System.out.println("How many people will split the budget");
 
-        ArrayList<User> userList = prepareUserList(scanner);
+        ArrayList<User> userList = prepareUserList(scanner);// User DB
+        ArrayList<Expense> expenseList = new ArrayList<>();// this is my Expense DB
         System.out.println("Added user account: " + userList.size());// how many user added
 
         String[] optionList = prepareOptionList();
@@ -31,25 +32,59 @@ public class Main {
                 case 0:
                     Expense expense = new Expense(); // creating object
                     System.out.println("Expense name: ");
-                    expense.expenseName= scanner.next();
+                    expense.expenseName = scanner.next();
                     System.out.println("Expense  Amount: ");
                     expense.amount = scanner.nextInt();
                     System.out.println("Which user make this expense? Just type user id: ");
-                   
-                   //show all users:id :0 name:ozzy
+
+                    //show all users:id :0 name:ozzy
                     for (User user : userList) {
                         System.out.println("id: " + userList.indexOf(user) + "name: " + user.name);
                     }
                     int userId = scanner.nextInt();
-
-
+                    User user = userList.get(userId);
+                    expense.user = user.name;
+                    expenseList.add(expense);
+                    break;
 
                 case 1:
-                    System.out.println("List Specific Person Expense");
-                    break;
-                case 2:
+                    System.out.println("Please provide user name that you would like to search");
 
-                    System.out.println("List all expenses");
+                    String userName = scanner.next();
+                    User myUser = null;
+                    for (User chosenUser : userList) {
+                        if(chosenUser.name.equals(userName)){
+                           myUser = chosenUser;
+                           break;
+                        }
+                    }
+                    if (myUser==null){
+                        System.out.println("User not exist");
+                        break;
+                    }
+                    //List Specific Person Expenses
+                    //0:expense amount : 100, expense by :Ozzy
+                    //1:expense amount : 100, expense by :Ozzy
+                    //ozzy spent $200
+
+                    int userExpenseAmount=0;
+                    int expenseCount=0;//will increase count when is if statement true
+
+                    for (int i = 0; i <expenseList.size() ; i++) {//
+                        if(expenseList.get(i).user.equals(userName)){
+                            userExpenseAmount += expenseList.get(i).amount;// to find the total amount
+                            expenseCount++;
+
+                            System.out.println(expenseCount + "expense amount" + expenseList.get(i).amount +", expense by :" + expenseList.get(i).user );
+                        }
+                    }
+                    System.out.println(myUser.name + "spent $ " + userExpenseAmount);
+                    break;
+
+                    case 2:
+                    for (int i = 0; i < expenseList.size() ; i++) {// to get all expenses
+                        System.out.println(i+ "expense amount:" + expenseList.get(i).amount+ "Expense by: " + expenseList.get(i).user);
+                    }
                     break;
                 case 3:
                     System.out.println("");
@@ -69,35 +104,32 @@ public class Main {
     }
 
 
+    //1. Create user
 
 
+    public static ArrayList<User> prepareUserList(Scanner scanner) {
 
-        //1. Create user
+        ArrayList<User> userList = new ArrayList<>();// we create arrayList to hold  all objects belongs to user class
 
+        int userCount = scanner.nextInt();// input 3 people
 
-       public  static ArrayList<User> prepareUserList(Scanner scanner) {
+        for (int i = 0; i < userCount; i++) {// for repeated actions
 
-           ArrayList<User> userList = new ArrayList<>();// we create arrayList to hold  all objects belongs to user class
+            User user = new User();// create user object
 
-           int userCount = scanner.nextInt();// input 3 people
+            System.out.println("Name: ");
+            user.name = scanner.next();
 
-           for (int i = 0; i < userCount; i++) {// for repeated actions
+            System.out.println("Email: ");
+            user.email = scanner.next();
 
-               User user = new User();// create user object
+            userList.add(user);// saving the object
 
-               System.out.println("Name: ");
-               user.name = scanner.next();
+        }
+        return userList;
+    }
 
-               System.out.println("Email: ");
-               user.email = scanner.next();
-
-               userList.add(user);// saving the object
-
-           }
-           return userList;
-       }
-
-       public static String [] prepareOptionList(){
+    public static String[] prepareOptionList() {
           /*
         //2. Menu Selection (Creating options) // will create aa
         0: Make Expense
@@ -107,10 +139,11 @@ public class Main {
         4: List all users
         5: Close the Budget
         */
-          String [] optionList  = {"Make Expense", "List Specific Person Expense", "List all expenses", "Make Split", "List all users", "Close the budget"};          ;
+        String[] optionList = {"Make Expense", "List Specific Person Expense", "List all expenses", "Make Split", "List all users", "Close the budget"};
+        ;
 
         return optionList;
-       }
+    }
 
 
 }
