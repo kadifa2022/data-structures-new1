4b0463ebdc7ee3723d5a93338795d697f30dcb17
Kadifa <Kudickadifa@gmail.com>
2023-07-15 12:17:27 -0500

    practice budget splitting app

---
+++ java-labs/src/main/java/co/cydeo/lab10_Inheritance_and_Abstraction/BusinessAnalyst.java
@@ -0,0 +1,37 @@
+package co.cydeo.lab10_Inheritance_and_Abstraction;
+
+public final class BusinessAnalyst extends Employee implements ScrumMember ,WorkFromHome{
+
+    public BusinessAnalyst(String name, int age, char gender, String id,  String salary) {
+        super(name, age, gender, id, "Business Analyst", salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+ "gathering requirement documents");
+    }
+
+    public void analyze(){
+        System.out.println(getName() + "is analyzing the requirements");
+    }
+
+
+    @Override
+    public void dailyStandUp() {
+        System.out.println();
+
+    }
+
+    @Override
+    public void springPlanning() {
+        System.out.println();
+
+    }
+
+    @Override
+    public void workFromHome() {
+        System.out.println();
+
+    }
+
+}
