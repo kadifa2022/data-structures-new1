27b0d5559683f279c5255e6670622db99e4331fd
Kadifa <Kudickadifa@gmail.com>
2023-07-15 17:55:27 -0500

    practice inheritance and abstraction

---
+++ java-labs/src/main/java/co/cydeo/lab11_exception_and_polymorphism/ExceptionPractice.java
@@ -0,0 +1,26 @@
+package co.cydeo.lab11_exception_and_polymorphism;
+
+import java.io.FileInputStream;
+
+public class ExceptionPractice {
+
+    public static void main(String[] args) {
+        String str = null;
+        try {
+            String result = str.repeat(3);
+
+            System.out.println(result);
+            System.out.println("Try block");
+
+        } catch (NullPointerException e) {// catch block is taking over
+
+            e.printStackTrace();
+            System.out.println("catch Block");
+            str = "Java";// as long is not null will be executed
+            String result = str.repeat(3);
+            System.out.println(result);
+        }
+        System.out.println("-------------------------------");
+
+    }
+}
