eaa3548b4a7791f2164b50235a980db344aa6560
Kadifa <Kudickadifa@gmail.com>
2023-07-16 06:17:16 -0500

    practice inheritance and abstraction

--- java-labs/src/main/java/co/cydeo/lab11_exception_and_polymorphism/restuarant_task/Server.java
+++ java-labs/src/main/java/co/cydeo/lab11_exception_and_polymorphism/restuarant_task/Server.java
@@ -18,4 +18,8 @@ public class Server extends Employee{
     public void cleanTable(){
         System.out.println(getName()+ "is cleaning tables");
     }
+    public Chef promoteToChef(){
+        return new Chef(getName(), getAge(),getGender(),getId(),"Head Chef", getSalary());
+
+    }
 }
