7405abec5eaa41021f171c82683befc2c32e8181
Kadifa <Kudickadifa@gmail.com>
2023-07-16 10:44:22 -0500

    practice inheritance and abstraction

--- dsa-05-Stacks/src/main/java/RecursiveFibonacci.java
+++ dsa-05-Stacks/src/main/java/RecursiveFibonacci.java
@@ -3,7 +3,7 @@ public class RecursiveFibonacci {
         System.out.println(fib(2));
         System.out.println(fib(5));
     }
-    public static int fib(int n){
+    public static int fib(int n){ //factorial method recursive
         if (n==0) return 0;// termination
         if (n==1) return 1;// termination
         return fib(n-1)+fib(n-2);
