a955dfc0ab87095e0b6ac400b1320f705c00b547
Kadifa <Kudickadifa@gmail.com>
2023-07-16 11:12:30 -0500

    practice inheritance and abstraction

--- dsa-upskill-codes/src/main/java/com/cydeo/stack/PowerOfTwo.java
+++ dsa-upskill-codes/src/main/java/com/cydeo/stack/PowerOfTwo.java
@@ -1,5 +1,25 @@
 package com.cydeo.stack;
 
-public class PowerOfTwo {
+public class  PowerOfTwo {
+    public  static boolean isPowerOfTwo(int n){
 
+        //1. base condition if i have n
+        if(n==1)return true;
+        if(n%2!=0 || n<=0)return false;
+
+        return isPowerOfTwo(n/2);
+    }
+
+
+/*
+power of 2
+int n = 16;
+ is power 2
+16/8 = 2
+8/4 = 2
+4/2 =2
+2/1 = 1
+
+
+ */
 }
