d9ad50dad069e538e1cf9225d8126a229c47b413
Kadifa <Kudickadifa@gmail.com>
2023-07-17 13:51:25 -0500

    practice inheritance and abstraction

---
+++ java-labs/src/main/java/co/cydeo/lab13_commerce_project/StaticConstants_MyDataBase.java
@@ -0,0 +1,10 @@
+package co.cydeo.lab13_commerce_project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticConstants_MyDataBase {
+
+    public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+
+}
