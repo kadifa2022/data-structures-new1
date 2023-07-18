ff7c483172db0d636eaff7fbe9f74053de0e4785
Kadifa <Kudickadifa@gmail.com>
2023-07-17 21:07:09 -0500

    practice inheritance and abstraction

---
+++ java-labs/src/main/java/co/cydeo/lab13_commerce_project/main.java
@@ -0,0 +1,15 @@
+package co.cydeo.lab13_commerce_project;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        DataGenerator.createCustomer();
+//
+//        DataGenerator.createProduct();
+//        DataGenerator.createCategory();
+//        DataGenerator.createBalance();
+//        DataGenerator.createDiscount();
+
+    }
+}
