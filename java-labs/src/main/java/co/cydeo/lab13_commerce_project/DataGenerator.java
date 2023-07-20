092b597d7f9344f99600946922466f7f451d0c28
Kadifa <Kudickadifa@gmail.com>
2023-07-19 19:11:13 -0500

    practice

--- java-labs/src/main/java/co/cydeo/lab13_commerce_project/DataGenerator.java
+++ java-labs/src/main/java/co/cydeo/lab13_commerce_project/DataGenerator.java
@@ -64,7 +64,7 @@ public class DataGenerator {
     }
     public static void createBalance(){
         Balance customerBalance= new CustomerBalance(StaticConstants.CUSTOMER_LIST.get(0).getId(), 100.00);
-        Balance giftCardBalance = new GiftCardBalance(StaticConstants.CUSTOMER_LIST.get(1).getId(), 100.00);
+        Balance giftCardBalance = new GiftCardBalance(StaticConstants.CUSTOMER_LIST.get(1).getId(), 200.00);
 
         StaticConstants.CUSTOMER_BALANCE_LIST.add(customerBalance);
         StaticConstants.GIFT_CARD_BALANCE_LIST.add(giftCardBalance);
