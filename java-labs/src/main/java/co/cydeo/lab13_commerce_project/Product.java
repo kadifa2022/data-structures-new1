033665c21c3d91d04e925f34c63cc60769eddb88
Kadifa <Kudickadifa@gmail.com>
2023-07-19 19:11:23 -0500

    practice

--- java-labs/src/main/java/co/cydeo/lab13_commerce_project/Product.java
+++ java-labs/src/main/java/co/cydeo/lab13_commerce_project/Product.java
@@ -4,13 +4,14 @@ package co.cydeo.lab13_commerce_project;
 
 import co.cydeo.lab13_commerce_project.category.Category;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Product {
     private UUID id;
     private String name;
     private Double price;
-    private Integer  Stock;
+    private Integer Stock;
     private Integer remainingStock;
     private UUID categoryId;// this id needs to mach with productId
 
@@ -48,11 +49,21 @@ public class Product {
     }
 
     public String getCategoryName() throws Exception {
-        for(Category category: StaticConstants.CATEGORY_LIST){
-            if (getCategoryId().toString().equals(category.getId().toString())){
+        for (Category category : StaticConstants.CATEGORY_LIST) {
+            if (getCategoryId().toString().equals(category.getId().toString())) {
                 return category.getName();
             }
         }
-        throw new Exception("Category not fount, " + getName()) ;// return null if does not exist
+        //exception need to be handled who ever call this method (main class case 1:)  and this message will be added  e.message
+        throw new Exception("Category not fount, " + getName());// return null if does not exist
+    }
+
+    public LocalDateTime getDeliveryDueDate() throws Exception {
+        for (Category category : StaticConstants.CATEGORY_LIST){
+            if(getCategoryId().toString().equals(category.getId().toString())){
+                return category.findDeliveryDueDate();
+            }
+        }
+        throw new Exception("Category could not find");
     }
 }
