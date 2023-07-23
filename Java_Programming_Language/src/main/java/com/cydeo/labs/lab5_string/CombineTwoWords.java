7cf294611825387bbee2db9005243f58381a516f
Kadifa <Kudickadifa@gmail.com>
2023-07-22 21:44:41 -0500

    practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/labs/lab5_string/CombineTwoWords.java
@@ -0,0 +1,25 @@
+package com.cydeo.labs.lab5_string;
+
+import java.util.Scanner;
+
+public class CombineTwoWords {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first word");
+        String firstWord = scan.next();
+        System.out.println("Enter your second word");
+        String secondWord = scan.next();
+        scan.close();
+        String result = "";
+
+        if(firstWord.charAt(firstWord.length() -1)==secondWord.charAt(0)){
+            result = firstWord + secondWord.substring(1);
+
+        }else{
+            result = firstWord + secondWord;
+        }
+        System.out.println(result);
+    }
+}
