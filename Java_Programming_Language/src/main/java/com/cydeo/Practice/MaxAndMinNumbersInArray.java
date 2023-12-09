a01631181326085f9b973313dd7348dd053ea8b4
Kadifa <Kudickadifa@gmail.com>
2023-12-08 22:29:00 -0600

    Arrays practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/MaxAndMinNumbersInArray.java
@@ -0,0 +1,21 @@
+package com.cydeo.Practice;
+
+public class MaxAndMinNumbersInArray {
+
+    public static void main(String[] args) {
+
+        int [] numbers= {2,4,6,-3,5};
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for(int i = 1; i <numbers.length; i++){
+            max= (numbers[i]>max) ? numbers[i] : max;
+            min= (numbers[i]<min) ? numbers[i] : min;
+        }
+        System.out.println("max= " + max + " min= " + min );
+
+
+    }
+
+
+}
