c4c9c48e222fd11055b5918b69fce1a56c50c362
Kadifa <Kudickadifa@gmail.com>
2023-12-08 22:29:13 -0600

    Arrays practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/MovingOddAndEvenNumbersInArray.java
@@ -0,0 +1,30 @@
+package com.cydeo.Practice;
+
+import java.util.Arrays;
+
+public class MovingOddAndEvenNumbersInArray {
+    public static void main(String[] args) {
+        int [] numbers = {1,2,4,5,7,9,-2,3};
+
+        int [] temp = new int[numbers.length];
+        int j = 0;
+        int k = numbers.length -1;
+
+        for(int i = 0; i <numbers.length; i++)
+
+            if(numbers[i] %2 !=0)
+                temp[j++] = numbers[i];
+            else
+                temp[k--] = numbers[i];
+            copyArray(temp, numbers);
+
+        System.out.println(Arrays.toString(numbers));
+        }
+
+    private static void copyArray(int[] temp, int[] numbers) {
+        for(int i =0; i< temp.length; i++)
+            numbers[i] = temp[i];
+
+    }
+}
+
