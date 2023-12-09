f5d1ff2113dd9ae505ae689f256e25bee45318e3
Kadifa <Kudickadifa@gmail.com>
2023-12-08 22:29:21 -0600

    Arrays practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/OccurrencesOfAnElementInArray.java
@@ -0,0 +1,24 @@
+package com.cydeo.Practice;
+
+public class OccurrencesOfAnElementInArray {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 1, 1, 2, 3, 4};
+        int searchElement = 1;
+        System.out.println(getNumberOcc(numbers,searchElement));
+    }
+
+
+
+    public static int getNumberOcc(int[]numbers, int searchElement){
+        int occ =0;
+        for (int i = 0; i < numbers.length; i++)
+            if (searchElement == numbers[i])
+                occ++;
+
+        return occ;
+
+
+        }
+
+    }
