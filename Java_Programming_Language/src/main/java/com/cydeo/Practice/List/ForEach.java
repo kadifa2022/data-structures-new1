521fa4c923b9f54fcd3bf4b3d4a344d9d0169a1d
Kadifa <Kudickadifa@gmail.com>
2023-12-09 13:30:48 -0600

    Arrays practice

--- Java_Programming_Language/src/main/java/com/cydeo/Practice/List/ForEach.java
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/List/ForEach.java
@@ -1,4 +1,26 @@
 package com.cydeo.Practice.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForEach {
+
+
+    public static void main(String[] args) {
+
+
+        List<Integer> arrList = new ArrayList<>();
+        arrList.add(5);
+        arrList.add(2);
+        arrList.add(2);
+        arrList.add(0);
+
+        int i = 0; // variable for index created
+
+        for (int each : arrList) {
+            System.out.println(each + " " + i);
+            i++;
+
+        }
+    }
 }
