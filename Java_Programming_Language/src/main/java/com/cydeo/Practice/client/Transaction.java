af312c4478b7ae082292bc91cb084ab407f3b489
Kadifa <Kudickadifa@gmail.com>
2023-12-10 20:06:38 -0600

    Arrays practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/client/Transaction.java
@@ -0,0 +1,37 @@
+package com.cydeo.Practice.client;
+
+import java.util.Date;
+
+public class Transaction {
+
+    private char type; //W, D
+    private double amount;
+    private double balance;
+    private Date date;
+    private String description;
+
+    public Transaction(char type, double amount, double balance, String description) {
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+        this.date = new Date();
+    }
+
+    public char getType() {
+        return type;
+    }
+
+
+    public String toString() {
+        return "Transaction{" +
+                "type=" + type +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                ", date=" + date +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
+
+
