af53883d1f59b5c181a8e10d2d7b2b6caee8fb40
Kadifa <Kudickadifa@gmail.com>
2024-01-07 19:43:47 -0600

    Arrays practice

---
+++ practice/src/main/java/com/cydeo/mosh/PracticeAlgorithm.java
@@ -0,0 +1,20 @@
+package com.cydeo.mosh;
+
+public class PracticeAlgorithm {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(numbers);
+
+
+    }
+
+
+
+
+
+    public void log(int [] numbers){// single operation in constant time
+        // (complexity O(1))
+        System.out.println(numbers [0]);
+    }
+}
