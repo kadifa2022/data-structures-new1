647178a9d3509838605cfc1b7bdf122decbe4af5
Kadifa <Kudickadifa@gmail.com>
2024-01-07 19:43:30 -0600

    Arrays practice

---
+++ Java_Programming_Language/src/main/java/com/cydeo/Practice/client/Test.java
@@ -0,0 +1,10 @@
+package com.cydeo.Practice.client;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+         str = "Word";
+        System.out.println(str);
+    }
+}
