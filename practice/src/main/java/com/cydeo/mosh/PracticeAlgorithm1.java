957a976f3e45b042187a41ba3be8065ae1bf03c7
Kadifa <Kudickadifa@gmail.com>
2024-01-08 19:14:45 -0600

    Arrays practice

--- practice/src/main/java/com/cydeo/mosh/PracticeAlgorithm1.java
+++ practice/src/main/java/com/cydeo/mosh/PracticeAlgorithm1.java
@@ -1,6 +1,6 @@
 package com.cydeo.mosh;
 
-<<<<<<< HEAD
+
 import com.cydeo.day2_arrays.Array;
 
 import java.util.Arrays;
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class PracticeAlgorithm1 {
 
     public static void main(String[] args) {
-        int[] numbers = {10,20,30}; // array
+        int[] numbers= {10, 20, 30}; // array
         System.out.println(numbers.length); // the size of the array 3 items
         System.out.println(Arrays.toString(numbers)); // prints array
 
@@ -31,23 +31,7 @@ public class PracticeAlgorithm1 {
         numbers3.removeAt(2);
         System.out.println(numbers3.indexOf(2));
         numbers3.print();
-=======
-public class PracticeAlgorithm {
 
-    public static void main(String[] args) {
-        int[] numbers = new int[3];
-        System.out.println(numbers);
-
->>>>>>> origin/master
-
+//
+        }
     }
-
-
-
-
-
-    public void log(int [] numbers){// single operation in constant time
-        // (complexity O(1))
-        System.out.println(numbers [0]);
-    }
-}
