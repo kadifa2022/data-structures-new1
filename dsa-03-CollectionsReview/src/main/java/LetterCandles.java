201eb5ab4c853150e4a7bb17372b0cfd0e9475f0
Kadifa <Kudickadifa@gmail.com>
2024-01-16 12:59:46 -0600

    Arrays practice

--- dsa-03-CollectionsReview/src/main/java/LetterCandles.java
+++ dsa-03-CollectionsReview/src/main/java/LetterCandles.java
@@ -36,19 +36,22 @@ public class LetterCandles {
         //2. minimize cost //
         //3. sort from
 
-        // create hashMap
+        // create hashMap //O(n<26)
         Map<Character,Integer > map = new HashMap<>();
         int count;
+        //O(n)
         for(Character ch: str.toCharArray()){
             if(map.containsKey(ch)){
                 count = map.get(ch);
                 map.put(ch,count +1);
             }else map.put(ch,1);
         }
+        //nlogn is not grater than 26 // constant value
         List<Integer> letterFreq = map.values().stream().sorted((o1,o2)-> o2.compareTo(o1)).collect(toList());
 
         // minimize cost
          int index = 0;
+         //O(n) n is not grater than 26 in fact it is constant number
          while(m > 0 && index < letterFreq.size()-1){
              if(letterFreq.get(index)>= letterFreq.get(index + 1)){
                  letterFreq.set(index, letterFreq.get(index)-1);
@@ -57,10 +60,11 @@ public class LetterCandles {
              else index ++;
          }
          // calculate min cost
+        //O(n) n is not grater than 26
         for(Integer num : letterFreq){
             cost+= num * num;
         }
-        //return result
+        //return result // over all is O(n)
         return cost;
     }
 
