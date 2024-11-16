d3293e6c5b163d88e4e21c717be53536b033c6b0
Kadifa <Kudickadifa@gmail.com>
2024-11-16 12:28:12 -0600

    binary search recursive

--- dsa-10-Search/src/main/java/searchReview/BinarySearch.java
+++ dsa-10-Search/src/main/java/searchReview/BinarySearch.java
@@ -3,13 +3,12 @@ package searchReview;
 public class BinarySearch {
     public static void main(String[] args) {
 
-        int [] num = new int[10];
-        for(int i = 0; i<10; i++){
-            num[i] =i;
+        int[] num = new int[10];
+        for (int i = 0; i < 10; i++) {
+            num[i] = i;
         }
         System.out.println(binarySearchIterative(num, 4));
     }
-
 
 
     public static int binarySearchIterative(int[] array, int data) {
@@ -20,8 +19,23 @@ public class BinarySearch {
             if (array[middle] == data) return middle;
             if (data < array[middle]) right = middle - 1;
             else left = middle + 1;
-
         }
         return -1;
     }
+
+    public static int binarySearchRecursive(int[] array, int data) {
+        return binarySearchRecursive(array, data, 0, array.length -1);
+    }
+
+    public static int binarySearchRecursive(int[] array, int data, int left, int right) {
+        if (left < right) return -1;
+        int middle = (left + right) / 2;
+        if (array[middle] == data) return middle;
+        if (data < array[middle]) return binarySearchRecursive(array, data, left, middle - 1);
+        else return binarySearchRecursive(array, data, middle + 1, right);
+
+
+}
+
+
 }
