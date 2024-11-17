337c129d4830ae86e6f4c49a6fb1cd637eba6b9c
Kadifa <Kudickadifa@gmail.com>
2024-11-17 08:00:48 -0600

    binary search practice

--- dsa-10-Search/src/main/java/searchReview/Task_ExperimentingSearchAlgorithm.java
+++ dsa-10-Search/src/main/java/searchReview/Task_ExperimentingSearchAlgorithm.java
@@ -2,7 +2,12 @@ package searchReview;
 // performanceCalculationOfSearchAlgorithm
 public class Task_ExperimentingSearchAlgorithm {
     public static void main(String[] args) {
-       // System.out.println(linearSearch());
+        int[] numbers = new int [1000000];
+        for(int i =0; i<1000000;i++){
+            numbers[i] = i;
+        }
+        long[] linearSearchResults= linearSearch(numbers, 333333);
+        System.out.println("Linear Search Result: " + linearSearchResults[0] + " milliseconds and " + linearSearchResults[1] + " comparisons.." );
 
     }
 
@@ -13,12 +18,32 @@ public class Task_ExperimentingSearchAlgorithm {
         for (int i = 0; i < array.length; i++) {
             comparisons ++;
             if(array[i]== data){
-                result [0] = System.currentTimeMillis()-startTime; // execution time
-                result[1] = comparisons; // number of comparisons
+                result [0] = System.currentTimeMillis()-startTime; //search time/ execution time
+                result [1] = comparisons; // number of comparisons
                 return result;
             }
         }
         return  new long[2]; // if we can't find return empty array
+    }
+    public static long [] binarySearchIterative(int[] array, int data){
+        long[] result = new long[2];
+        long startTime = System.currentTimeMillis();
+        int comparisons =0;
+        int left = 0;
+        int right =array.length-1;
+        while(left <= right){
+
+        int  middle = (left + right)/2;
+        comparisons ++;
+        if(array[middle] == data) {
+            result [0] = System.currentTimeMillis() -startTime;
+            result [1] = comparisons;
+            return  result;}
+        comparisons ++;
+            if(data<array[middle]) right = middle-1;
+            else left = middle +1;
+        }
+        return  new long[2];
     }
 
 }
