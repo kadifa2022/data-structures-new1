7225ccff61c9641c62d04d424f22eea0569c7f79
Kadifa <Kudickadifa@gmail.com>
2024-11-18 13:07:14 -0600

    find second largest element

---
+++ practice_coding/src/main/java/practice1/FindTheSecondLargestNumber.java
@@ -0,0 +1,28 @@
+package practice1;
+
+public class FindTheSecondLargestNumber{
+    public static void main(String[] args) {
+        int [] numbers = {10,7,23,45,8};
+        int secondLargest = findSecondLargest(numbers);
+        System.out.println(secondLargest);
+
+    }
+    public static int findSecondLargest(int [] array){
+        int largest = Integer.MIN_VALUE;// or 0
+        int secondLargest = Integer.MIN_VALUE;// or 0
+
+        for(int i = 0; i < array.length; i++){
+            int num = array[i];
+            if(largest < num){
+                secondLargest = largest;
+                largest = num;
+            }// {10,7,23,45,30}; // complex case
+            else if (num > secondLargest && num != largest){
+                secondLargest = num;
+
+            }
+        }
+        return secondLargest;
+    }
+
+}
