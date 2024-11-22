c2cc1850a3360ac84fedfac621de4a074f89ceee
Kadifa <Kudickadifa@gmail.com>
2024-11-21 19:40:48 -0600

    Bubble sort

---
+++ practice_coding/src/main/java/java/Employee.java
@@ -0,0 +1,14 @@
+package java;
+
+public class Employee {
+
+    public int exp;
+
+    public Employee() {}
+    public   double  calculateSalary(){
+        int salary = exp * 50000;
+        return salary;
+    }
+
+
+}
---
+++ practice_coding/src/main/java/practice1/SumOfArrayElements2.java
@@ -0,0 +1,19 @@
+package practice1;
+
+
+
+public class SumOfArrayElements2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+        int sum = calculateSum(numbers);
+        System.out.println(sum);
+    }
+
+    public static int calculateSum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];// add each element to the sum
+        }
+        return sum;
+    }
+}
