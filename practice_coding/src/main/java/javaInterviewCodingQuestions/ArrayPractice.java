340209c4693fb4594e5c97ac31a43d0db13bac4f
Kadifa <Kudickadifa@gmail.com>
2024-12-09 14:32:10 -0600

    practice coding

---
+++ practice_coding/src/main/java/javaInterviewCodingQuestions/ArrayPractice.java
@@ -0,0 +1,21 @@
+package javaInterviewCodingQuestions;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+        int [] nums= new int[3]; //
+        nums[0]=10;
+        nums[1]=20;
+        nums[2]=30;
+
+        System.out.println(Arrays.toString(nums));
+        int[]scores={10,20,30,40,50};// creating array with literals
+       //int[] scores2= new int[]{10,20,30,40,50}; // same as above
+
+        System.out.println("Multi dimential array");
+        int [][] arr2D = {};// For 1-D Array
+        int [][][] arr3D = {};
+    }
+}
